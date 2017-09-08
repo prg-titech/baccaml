3a5918068ed42c06337f9953216668345a451f3d
Yusuke Izawa <yuizalp@gmail.com>
2017-09-08 21:38:56 +0900

    WIP: update emitVirtual.ml

--- emitVirtual.ml
+++ emitVirtual.ml
@@ -9,11 +9,54 @@ let to_string_id_or_imm id_or_imm =
   | V (t) -> t
   | C (i) -> Pervasives.string_of_int i
 
-let print_exp exp =
+let rec to_string_type typ =
+  let rec to_string_type_list lst =
+    match lst with
+    | [] -> ""
+    | h :: t -> (to_string_type h) ^ ", " ^ to_string_type_list t
+  in
+  match typ with
+  | Type.Unit -> "Unit"
+  | Type.Bool -> "Bool"
+  | Type.Int -> "Int"
+  | Type.Float -> "Float"
+  | Type.Fun (hd :: tl, t) ->"Fun (" ^ (to_string_type hd) ^ ", " ^  (to_string_type_list tl) ^ ", " ^ (to_string_type t) ^ ")"
+  | Type.Tuple (hd :: tl) -> "Tuple (" ^ (to_string_type hd) ^ ", " ^ (to_string_type_list tl) ^ ")"
+  | Type.Array (t) -> "Array (" ^ (to_string_type t) ^ ")"
+  | Type.Var (x) ->
+     match !x with
+     | None -> "Var (ref None)"
+     | Some v -> "Var ( ref Some (" ^ (to_string_type v) ^ ")"
+
+let rec to_string_exp exp =
   match exp with
-  | Nop -> Printf.printf "Nop"
-  | Set (i) -> Printf.printf "Set (%d)" i
-  | SetL (l) -> Printf.printf "SetL (%s)" (to_string_id l)
-  | Mov (t)-> Printf.printf "Mov (%s)" t
-  | Neg (t)-> Printf.printf "Neg (%s)" t
-  | Add (t, id_or_imm) -> Printf.printf "Add (%s, %s)" t (to_string_id_or_imm id_or_imm)
+  | Nop -> "Nop"
+  | Set (i) -> Printf.sprintf "Set (%d)" i
+  | SetL (l) -> Printf.sprintf "SetL (%s)" (to_string_id l)
+  | Mov (x')-> Printf.sprintf "Mov (%s)" x'
+  | Neg (x')-> Printf.sprintf "Neg (%s)" x'
+  | Add (x', y') -> Printf.sprintf "Add (%s, %s)" x' (to_string_id_or_imm y')
+  | Sub (x', y') -> Printf.sprintf "Sub (%s, %s)" x' (to_string_id_or_imm y')
+  | Ld (x', y', i) -> Printf.sprintf "Ld (%s, %s %d)" x' (to_string_id_or_imm y') i
+  | St (x1, x2, y', i) -> Printf.sprintf "St (%s, %s, %s, %d)" x1 x2 (to_string_id_or_imm y') i
+  | FMovD (x') -> Printf.sprintf "FMovd (%s)" x'
+  | FNegD (x') -> Printf.sprintf "FNegD (%s)" x'
+  | FAddD (x', y') -> Printf.sprintf "FAddD (%s, %s)" x' y'
+  | FSubD (x', y') -> Printf.sprintf "FSubD (%s, %s)" x' y'
+  | FMulD (x', y') -> Printf.sprintf "FMulD (%s, %s)" x' y'
+  | FDivD (x', y') -> Printf.sprintf "FDivD (%s, %s)" x' y'
+  | LdDF (x', y', i) ->
+     let id_or_imm = to_string_id_or_imm y' in
+     Printf.sprintf "LdDF (%s, %s, %d)" x' id_or_imm i
+  | StDF (x1, x2, y', i) ->
+     let id_or_imm = to_string_id_or_imm y' in
+     Printf.sprintf "StDf (%s, %s, %s, %d)" x1 x2 id_or_imm i
+  | Comment (s) -> Printf.sprintf "Comment (%s)" s
+    
+let rec to_string_t t =
+  match t with
+  | Ans (exp) ->
+     to_string_exp exp
+
+let print_exp exp =
+  Printf.printf "%s" (to_string_exp exp)
