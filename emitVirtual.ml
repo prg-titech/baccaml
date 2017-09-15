73509340472a62866c63fa7d1a028767ca94be25
Yusuke Izawa <yuizalp@gmail.com>
2017-09-15 18:37:06 +0900

    Revert "Add \n"
    This reverts commit 0ec06b5ecdc94eea3841597a2bfacf154dd7c714.

--- emitVirtual.ml
+++ emitVirtual.ml
@@ -9,27 +9,27 @@ let to_string_id_or_imm id_or_imm =
   | V (t) -> t
   | C (i) -> Pervasives.string_of_int i
 
-let rec to_string_type typ =  
+let rec to_string_type typ =
   let rec to_string_type_list lst =
     match lst with
     | [] -> ""
     | h :: t -> (to_string_type h) ^ ", " ^ to_string_type_list t
   in
   match typ with
-  | Type.Unit -> "Unit\n"
-  | Type.Bool -> "Bool\n"
-  | Type.Int -> "Int\n"
-  | Type.Float -> "Float\n"
+  | Type.Unit -> "Unit"
+  | Type.Bool -> "Bool"
+  | Type.Int -> "Int"
+  | Type.Float -> "Float"
   | Type.Fun (hd :: tl, t) ->
-     Printf.sprintf "Fun (%s, %s, %s)\n" (to_string_type hd) (to_string_type_list tl) (to_string_type t)
+     Printf.sprintf "Fun (%s, %s, %s)" (to_string_type hd) (to_string_type_list tl) (to_string_type t)
   | Type.Tuple (hd :: tl) ->
-     Printf.sprintf "Tuple (%s, %s)\n" (to_string_type hd) (to_string_type_list tl)
+     Printf.sprintf "Tuple (%s, %s)" (to_string_type hd) (to_string_type_list tl)
   | Type.Array (t) ->
-     Printf.sprintf "Array (%s)\n" (to_string_type t)
+     Printf.sprintf "Array (%s)" (to_string_type t)
   | Type.Var (x) ->
      match !x with
-     | None -> "Var (ref None)\n"
-     | Some v -> Printf.sprintf "Var (ref Some (%s))\n" (to_string_type v)
+     | None -> "Var (ref None)"
+     | Some v -> Printf.sprintf "Var (ref Some (%s))" (to_string_type v)
 
 
 let rec to_string_ids ids =
@@ -41,67 +41,67 @@ let rec to_string_ids ids =
 (* Asm.exp to string *)
 let rec to_string_exp exp =
   match exp with
-  | Nop -> "Nop\n"
-  | Set (i) -> Printf.sprintf "Set (%d)\n" i
-  | SetL (l) -> Printf.sprintf "SetL (%s)\n" (to_string_idl l)
-  | Mov (x')-> Printf.sprintf "Mov (%s)\n" x'
-  | Neg (x')-> Printf.sprintf "Neg (%s)\n" x'
-  | Add (x', y') -> Printf.sprintf "Add (%s, %s)\n" x' (to_string_id_or_imm y')
-  | Sub (x', y') -> Printf.sprintf "Sub (%s, %s)\n" x' (to_string_id_or_imm y')
-  | Ld (x', y', i) -> Printf.sprintf "Ld (%s, %s %d)\n" x' (to_string_id_or_imm y') i
-  | St (x1, x2, y', i) -> Printf.sprintf "St (%s, %s, %s, %d)\n" x1 x2 (to_string_id_or_imm y') i
-  | FMovD (x') -> Printf.sprintf "FMovd (%s)\n" x'
-  | FNegD (x') -> Printf.sprintf "FNegD (%s)\n" x'
-  | FAddD (x', y') -> Printf.sprintf "FAddD (%s, %s)\n" x' y'
-  | FSubD (x', y') -> Printf.sprintf "FSubD (%s, %s)\n" x' y'
-  | FMulD (x', y') -> Printf.sprintf "FMulD (%s, %s)\n" x' y'
-  | FDivD (x', y') -> Printf.sprintf "FDivD (%s, %s)\n" x' y'
+  | Nop -> "Nop"
+  | Set (i) -> Printf.sprintf "Set (%d)" i
+  | SetL (l) -> Printf.sprintf "SetL (%s)" (to_string_idl l)
+  | Mov (x')-> Printf.sprintf "Mov (%s)" x'
+  | Neg (x')-> Printf.sprintf "Neg (%s)" x'
+  | Add (x', y') -> Printf.sprintf "Add (%s, %s)" x' (to_string_id_or_imm y')
+  | Sub (x', y') -> Printf.sprintf "Sub (%s, %s)" x' (to_string_id_or_imm y')
+  | Ld (x', y', i) -> Printf.sprintf "Ld (%s, %s %d)" x' (to_string_id_or_imm y') i
+  | St (x1, x2, y', i) -> Printf.sprintf "St (%s, %s, %s, %d)" x1 x2 (to_string_id_or_imm y') i
+  | FMovD (x') -> Printf.sprintf "FMovd (%s)" x'
+  | FNegD (x') -> Printf.sprintf "FNegD (%s)" x'
+  | FAddD (x', y') -> Printf.sprintf "FAddD (%s, %s)" x' y'
+  | FSubD (x', y') -> Printf.sprintf "FSubD (%s, %s)" x' y'
+  | FMulD (x', y') -> Printf.sprintf "FMulD (%s, %s)" x' y'
+  | FDivD (x', y') -> Printf.sprintf "FDivD (%s, %s)" x' y'
   | LdDF (x', y', i) ->
      let id_or_imm = to_string_id_or_imm y' in
-     Printf.sprintf "LdDF (%s, %s, %d)\n" x' id_or_imm i
+     Printf.sprintf "LdDF (%s, %s, %d)" x' id_or_imm i
   | StDF (x1, x2, y', i) ->
      let id_or_imm = to_string_id_or_imm y' in
-     Printf.sprintf "StDf (%s, %s, %s, %d)\n" x1 x2 id_or_imm i
-  | Comment (s) -> Printf.sprintf "Comment (%s)\n" s
+     Printf.sprintf "StDf (%s, %s, %s, %d)" x1 x2 id_or_imm i
+  | Comment (s) -> Printf.sprintf "Comment (%s)" s
   (* virtual instructions *)
   | IfEq (x', y', e1, e2) ->
      let t1 = to_string_t e1 in
      let t2 = to_string_t e2 in
-     Printf.sprintf "IfEq (%s, %s, %s, %s)\n" x' (to_string_id_or_imm y') t1 t2
+     Printf.sprintf "IfEq (%s, %s, %s, %s)" x' (to_string_id_or_imm y') t1 t2
   | IfLE (x', y', e1, e2) ->
      let t1 = to_string_t e1 in
      let t2 = to_string_t e2 in
-     Printf.sprintf "IfLE (%s, %s, %s, %s)\n" x' (to_string_id_or_imm y') t1 t2
+     Printf.sprintf "IfLE (%s, %s, %s, %s)" x' (to_string_id_or_imm y') t1 t2
   | IfGE (x', y', e1, e2) ->
      let t1 = to_string_t e1 in
      let t2 = to_string_t e2 in
-     Printf.sprintf "IfGE (%s, %s, %s, %s)\n" x' (to_string_id_or_imm y') t1 t2
+     Printf.sprintf "IfGE (%s, %s, %s, %s)" x' (to_string_id_or_imm y') t1 t2
   | IfFEq (x1, x2, e1, e2) ->
      let t1 = to_string_t e1 in
      let t2 = to_string_t e2 in
-     Printf.sprintf "IfFEq (%s, %s, %s, %s)\n" x1 x2 t1 t2
+     Printf.sprintf "IfFEq (%s, %s, %s, %s)" x1 x2 t1 t2
   | IfFLE (x1, x2, e1, e2) ->
      let t1 = to_string_t e1 in
      let t2 = to_string_t e2 in
-     Printf.sprintf "IfFLE (%s, %s, %s, %s)\n" x1 x2 t1 t2
+     Printf.sprintf "IfFLE (%s, %s, %s, %s)" x1 x2 t1 t2
   | CallCls (x', ids1, ids2) ->
-     Printf.sprintf "CallCls (%s, %s, %s)\n" x' (to_string_ids ids1) (to_string_ids ids2)
+     Printf.sprintf "CallCls (%s, %s, %s)" x' (to_string_ids ids1) (to_string_ids ids2)
   | CallDir (l', ids1, ids2) ->
-     Printf.sprintf "CallCls (%s, %s, %s)\n" (to_string_idl l') (to_string_ids ids1) (to_string_ids ids2)
+     Printf.sprintf "CallCls (%s, %s, %s)" (to_string_idl l') (to_string_ids ids1) (to_string_ids ids2)
   | Save (x1, x2) ->
-     Printf.sprintf "Save (%s, %s)\n" x1 x2
+     Printf.sprintf "Save (%s, %s)" x1 x2
   | Restore x ->
-     Printf.sprintf "Restore (%s)\n" x
+     Printf.sprintf "Restore (%s)" x
 
 (* Asm.t to string *)
 and to_string_t t =
   match t with
   | Ans (exp) ->
-     Printf.sprintf "Ans (%s)\n" (to_string_exp exp)
+     Printf.sprintf "Ans (%s)" (to_string_exp exp)
   | Let (x', exp, t') ->
      match x' with
      | (id', type') ->
-        Printf.sprintf "Let ((%s, %s), %s, %s)\n" id' (to_string_type type') (to_string_exp exp) (to_string_t t')
+        Printf.sprintf "Let ((%s, %s), %s, %s)" id' (to_string_type type') (to_string_exp exp) (to_string_t t')
 
 (* fundef to string *)
 let to_string_fundef fundef' =
@@ -112,7 +112,7 @@ let to_string_fundef fundef' =
      let fargs_str = to_string_ids f in
      let body_str = to_string_t b in
      let ret_str = to_string_type r in
-     Printf.sprintf "{name = %s; args = %s; fargs = %s; body = %s; ret = %s}\n" name_str args_str fargs_str body_str ret_str
+     Printf.sprintf "{name = %s; args = %s; fargs = %s; body = %s; ret = %s}" name_str args_str fargs_str body_str ret_str
 
 let rec to_string_floating_point_table lst =
   match lst with
@@ -128,7 +128,7 @@ let to_string_prog p =
      let table = to_string_floating_point_table xs in
      let toplevel_function = String.concat " " (List.map to_string_fundef fundefs) in
      let main_exp = to_string_t t' in
-     Printf.sprintf "Prog (%s, %s, %s)\n" table toplevel_function main_exp
+     Printf.sprintf "Prog (%s, %s, %s)" table toplevel_function main_exp
 
 (* entry point *)
 let f oc asm_prog =
