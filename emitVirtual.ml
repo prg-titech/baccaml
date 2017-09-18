7fd56f87da0c5df77cf7deecf17d87a24e056186
Yusuke Izawa <yuizalp@gmail.com>
2017-09-18 19:07:55 +0900

    Fix indentation

--- emitVirtual.ml
+++ emitVirtual.ml
@@ -21,15 +21,15 @@ let rec to_string_type typ =
   | Type.Int -> "Int"
   | Type.Float -> "Float"
   | Type.Fun (hd :: tl, t) ->
-     Printf.sprintf "Fun (%s, %s, %s)" (to_string_type hd) (to_string_type_list tl) (to_string_type t)
+    Printf.sprintf "Fun (%s, %s, %s)" (to_string_type hd) (to_string_type_list tl) (to_string_type t)
   | Type.Tuple (hd :: tl) ->
-     Printf.sprintf "Tuple (%s, %s)" (to_string_type hd) (to_string_type_list tl)
+    Printf.sprintf "Tuple (%s, %s)" (to_string_type hd) (to_string_type_list tl)
   | Type.Array (t) ->
-     Printf.sprintf "Array (%s)" (to_string_type t)
+    Printf.sprintf "Array (%s)" (to_string_type t)
   | Type.Var (x) ->
-     match !x with
-     | None -> "Var (ref None)"
-     | Some v -> Printf.sprintf "Var (ref Some (%s))" (to_string_type v)
+    match !x with
+    | None -> "Var (ref None)"
+    | Some v -> Printf.sprintf "Var (ref Some (%s))" (to_string_type v)
 
 
 let rec to_string_ids ids =
@@ -57,76 +57,76 @@ let rec to_string_exp exp =
   | FMulD (x', y') -> Printf.sprintf "FMulD (%s, %s)" x' y'
   | FDivD (x', y') -> Printf.sprintf "FDivD (%s, %s)" x' y'
   | LdDF (x', y', i) ->
-     let id_or_imm = to_string_id_or_imm y' in
-     Printf.sprintf "LdDF (%s, %s, %d)" x' id_or_imm i
+    let id_or_imm = to_string_id_or_imm y' in
+    Printf.sprintf "LdDF (%s, %s, %d)" x' id_or_imm i
   | StDF (x1, x2, y', i) ->
-     let id_or_imm = to_string_id_or_imm y' in
-     Printf.sprintf "StDf (%s, %s, %s, %d)" x1 x2 id_or_imm i
+    let id_or_imm = to_string_id_or_imm y' in
+    Printf.sprintf "StDf (%s, %s, %s, %d)" x1 x2 id_or_imm i
   | Comment (s) -> Printf.sprintf "Comment (%s)" s
   (* virtual instructions *)
   | IfEq (x', y', e1, e2) ->
-     let t1 = to_string_t e1 in
-     let t2 = to_string_t e2 in
-     Printf.sprintf "IfEq (%s, %s, %s, %s)" x' (to_string_id_or_imm y') t1 t2
+    let t1 = to_string_t e1 in
+    let t2 = to_string_t e2 in
+    Printf.sprintf "IfEq (%s, %s, %s, %s)" x' (to_string_id_or_imm y') t1 t2
   | IfLE (x', y', e1, e2) ->
-     let t1 = to_string_t e1 in
-     let t2 = to_string_t e2 in
-     Printf.sprintf "IfLE (%s, %s, %s, %s)" x' (to_string_id_or_imm y') t1 t2
+    let t1 = to_string_t e1 in
+    let t2 = to_string_t e2 in
+    Printf.sprintf "IfLE (%s, %s, %s, %s)" x' (to_string_id_or_imm y') t1 t2
   | IfGE (x', y', e1, e2) ->
-     let t1 = to_string_t e1 in
-     let t2 = to_string_t e2 in
-     Printf.sprintf "IfGE (%s, %s, %s, %s)" x' (to_string_id_or_imm y') t1 t2
+    let t1 = to_string_t e1 in
+    let t2 = to_string_t e2 in
+    Printf.sprintf "IfGE (%s, %s, %s, %s)" x' (to_string_id_or_imm y') t1 t2
   | IfFEq (x1, x2, e1, e2) ->
-     let t1 = to_string_t e1 in
-     let t2 = to_string_t e2 in
-     Printf.sprintf "IfFEq (%s, %s, %s, %s)" x1 x2 t1 t2
+    let t1 = to_string_t e1 in
+    let t2 = to_string_t e2 in
+    Printf.sprintf "IfFEq (%s, %s, %s, %s)" x1 x2 t1 t2
   | IfFLE (x1, x2, e1, e2) ->
-     let t1 = to_string_t e1 in
-     let t2 = to_string_t e2 in
-     Printf.sprintf "IfFLE (%s, %s, %s, %s)" x1 x2 t1 t2
+    let t1 = to_string_t e1 in
+    let t2 = to_string_t e2 in
+    Printf.sprintf "IfFLE (%s, %s, %s, %s)" x1 x2 t1 t2
   | CallCls (x', ids1, ids2) ->
-     Printf.sprintf "CallCls (%s, %s, %s)" x' (to_string_ids ids1) (to_string_ids ids2)
+    Printf.sprintf "CallCls (%s, %s, %s)" x' (to_string_ids ids1) (to_string_ids ids2)
   | CallDir (l', ids1, ids2) ->
-     Printf.sprintf "CallCls (%s, %s, %s)" (to_string_idl l') (to_string_ids ids1) (to_string_ids ids2)
+    Printf.sprintf "CallCls (%s, %s, %s)" (to_string_idl l') (to_string_ids ids1) (to_string_ids ids2)
   | Save (x1, x2) ->
-     Printf.sprintf "Save (%s, %s)" x1 x2
+    Printf.sprintf "Save (%s, %s)" x1 x2
   | Restore x ->
-     Printf.sprintf "Restore (%s)" x
+    Printf.sprintf "Restore (%s)" x
 
 (* Asm.t to string *)
 and to_string_t t =
   match t with
   | Ans (exp) ->
-     Printf.sprintf "Ans (%s)" (to_string_exp exp)
+    Printf.sprintf "Ans (%s)" (to_string_exp exp)
   | Let (x', exp, t') ->
-     match x' with
-     | (id', type') ->
-        Printf.sprintf "Let ((%s, %s), %s, %s)" id' (to_string_type type') (to_string_exp exp) (to_string_t t')
+    match x' with
+    | (id', type') ->
+      Printf.sprintf "Let ((%s, %s), %s, %s)" id' (to_string_type type') (to_string_exp exp) (to_string_t t')
 
 (* fundef to string *)
 let to_string_fundef fundef' =
   match fundef' with
   | { name = n; args = a; fargs = f; body = b; ret = r } ->
-     let name_str = to_string_idl n in
-     let args_str = to_string_ids a in
-     let fargs_str = to_string_ids f in
-     let body_str = to_string_t b in
-     let ret_str = to_string_type r in
-     Printf.sprintf "{name = %s; args = %s; fargs = %s; body = %s; ret = %s}" name_str args_str fargs_str body_str ret_str
+    let name_str = to_string_idl n in
+    let args_str = to_string_ids a in
+    let fargs_str = to_string_ids f in
+    let body_str = to_string_t b in
+    let ret_str = to_string_type r in
+    Printf.sprintf "{name = %s; args = %s; fargs = %s; body = %s; ret = %s}" name_str args_str fargs_str body_str ret_str
 
 let rec to_string_floating_point_table lst =
   match lst with
   | [] -> ""
   | hd :: tl ->
-     let (idl, f) = hd in
-     (to_string_idl idl) ^ (Pervasives.string_of_float f) ^ (to_string_floating_point_table lst)
+    let (idl, f) = hd in
+    (to_string_idl idl) ^ (Pervasives.string_of_float f) ^ (to_string_floating_point_table lst)
 
 (* Asm.prog to string *)
 let to_string_prog p =
   match p with
   | Prog (xs, fundefs, t') ->
-     let main_exp = to_string_t t' in
-     Printf.sprintf "Prog (%s)" main_exp
+    let main_exp = to_string_t t' in
+    Printf.sprintf "Prog (%s)" main_exp
 
 (* entry point *)
 let f oc asm_prog =
