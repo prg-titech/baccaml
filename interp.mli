01e4f6339354a0a7bac35a85919f19eef5137e84
Yusuke Izawa <yuizalp@gmail.com>
2017-09-29 16:25:52 +0900

    ocamlc -i interp.ml > interp.mli

--- interp.mli
+++ interp.mli
@@ -1,18 +1,22 @@
 type instruction =
-    Mov
-  | Add
-  | Cmp
-  | Div
-  | Sub
-  | And
-  | Neg
-  | Not
-  | Or
-  | Jmp
-  | Jne
-  | Call
-  | Ret
-  | Pop
-  | Push
+    Mov of int * int
+  | MovImm of int * int
+  | Add of int * int
+  | AddImm of int * int
+  | Cmp of int * int
+  | CmpImm of int * int
+  | Div of int
+  | DivImm of int
+  | Sub of int * int
+  | SubImm of int * int
+  | And of int * int
+  | Or of int * int
+  | Jump of int
+  | Jne of int
   | Nop
-val interp : instruction array -> int array -> int -> int -> 'a
+  | Load of int * int
+  | Store of int * int
+  | Halt
+val interp : instruction array -> int array -> int array -> int -> int array
+val add1 : instruction array
+val sum1 : instruction array
