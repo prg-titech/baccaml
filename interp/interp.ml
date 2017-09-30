cbf34312280ffc050f21300fc133525df0031a93
Yusuke Izawa <yuizalp@gmail.com>
2017-09-30 20:15:33 +0900

    Update interp.ml and interpSpec.ml

--- interp/interp.ml
+++ interp/interp.ml
@@ -1,3 +1,5 @@
+open Core.Std
+
 (* x86 命令セット *)
 (* オペランドの順番は GAS 形式 *)
 type instruction =
@@ -14,6 +16,7 @@ type instruction =
   | And of int * int
   | Or of int * int
   | Jump of int
+  | JumpIf of int
   | Jne of int
   | Nop
   | Load of int * int
@@ -21,85 +24,91 @@ type instruction =
   | Halt
 ;;
 
-let rec interp (program : instruction array) (reg : int array) (mem : int array) (pc : int) : 'a =
+let rec interp (program : instruction array) (reg : int array) (mem : int array) (flag : int array) (pc : int) : int array =
   match program.(pc) with
   | Mov (x, y) ->
     let op1 = reg.(x) in
     reg.(y) <- op1;
-    interp program reg mem (pc + 1)
+    interp program reg mem flag (pc + 1)
   | MovImm (x, y) ->
     reg.(y) <- x;
-    interp program reg mem (pc + 1)
+    interp program reg mem flag (pc + 1)
   | Add (x, y) ->
     let r1 = reg.(x) in
     let r2 = reg.(y) in
     reg.(y) <- (r1 + r2);
-    interp program reg mem (pc + 1)
+    interp program reg mem flag (pc + 1)
   | AddImm (x, y) ->
     let r2 = reg.(y) in
     reg.(y) <- (x + r2);
-    interp program reg mem (pc + 1)
+    interp program reg mem flag (pc + 1)
   | Cmp (x, y) ->
     let r1 = reg.(x) in
     let r2 = reg.(y) in
-    if r1 < r2 then reg.(y) <- 1
-    else reg.(y) <- 0;
-    interp program reg mem (pc + 1)
+    if r1 < r2 then flag.(0) <- 1
+    else if r1 = r2 then flag.(1) <- 1
+    else flag.(0) <- 0;
+    interp program reg mem flag (pc + 1)
   | CmpImm (x, y) ->
     let r2 = reg.(y) in
-    if x < r2 then reg.(y) <- 1
-    else reg.(y) <- 0;
-    interp program mem reg (pc + 1)
+    if x < r2 then flag.(0) <- 1
+    else if x = r2 then flag.(1) <- 1
+    else flag.(0) <- 0;
+    interp program mem reg flag (pc + 1)
   | Div x ->
     let r1 = reg.(x) in
     reg.(0) <- (reg.(0) / r1);
     reg.(1) <- (reg.(0) mod r1);
-    interp program reg mem (pc + 1)
+    interp program reg mem flag (pc + 1)
   | DivImm x ->
     reg.(0) <- (reg.(0) / x);
     reg.(1) <- (reg.(0) mod x);
-    interp program reg mem (pc + 1)
+    interp program reg mem flag (pc + 1)
   | Sub (x, y) ->
     let r1 = reg.(x) in
     let r2 = reg.(y) in
     reg.(y) <- (r2 - r1);
-    interp program reg mem (pc + 1)
+    interp program reg mem flag (pc + 1)
   | SubImm (x, y) ->
     let r2 = reg.(y) in
     reg.(y) <- (r2 - y);
-    interp program reg mem (pc + 1)
+    interp program reg mem flag (pc + 1)
   | And (x, y) ->
     let r1 = reg.(x) in
     let r2 = reg.(y) in
     if r1 = 1 && r2 = 1 then reg.(y) <- 1
     else reg.(y) <- 0;
-    interp program reg mem (pc + 1)
+    interp program reg mem flag (pc + 1)
   | Or (x, y) ->
     let r1 = reg.(x) in
     let r2 = reg.(y) in
     if r1 = 1 || r2 = 1 then reg.(y) <- 1
     else reg.(y) <- 0;
-    interp program reg mem (pc + 1)
+    interp program reg mem flag (pc + 1)
   | Jump address ->
-    interp program reg mem address
-  | Jne address ->
-    let flag = reg.(0) in
-    if flag = 0 then
-      interp program reg mem address
+    interp program reg mem flag address
+  | JumpIf address ->
+    if (flag.(0)) = 1 then
+      interp program reg mem flag address
     else
-      interp program reg mem (pc + 1)
+      interp program reg mem flag address
+  | Jne address ->
+    if (flag.(1)) = 0 then
+      interp program reg mem flag address
+    else
+      interp program reg mem flag (pc + 1)
   | Nop ->
-    interp program reg mem (pc + 1)
+    interp program reg mem flag (pc + 1)
   | Load (x, y) ->
     let r1 = reg.(x) in
     let m1 = mem.(r1) in
     reg.(y) <- m1;
-    interp program reg mem (pc + 1)
+    interp program reg mem flag (pc + 1)
   | Store (x, y) ->
     let r1 = reg.(x) in
     let r2 = reg.(y) in
     mem.(r2) <- r1;
-    interp program reg mem (pc + 1)
+    interp program reg mem flag (pc + 1)
   | Halt ->
     reg
 ;;
--- interp/interpSpec.ml
+++ interp/interpSpec.ml
@@ -1,50 +1,62 @@
 open Interp
+open OUnit
 
-(*
-# r0 <- n
-# r1 <- 0
-# r2 <- 0
-
-l1:
-  r1 += r2;
-  r2 += 1;
-  if (r2 != r0) goto l1;
+(* sample asm program for sum_n
+ *# r0 <- n
+ *# r1 <- 0
+ *# r2 <- 0
+ *
+ * l1:
+ * r1 += r2;
+ * r2 += 1;
+ * if (r2 != r0) goto l1;
+   r1 += r2;
 *)
-let sum_n = [| MovImm(0, 1); MovImm(0, 2); Add(2, 1); AddImm(1, 2); Cmp(0, 2); Jne(2); Halt |]
-;;
 
-let print_res reg =
-  print_int reg.(1)
-;;
+(* Test Fixture *)
+let test_fixture = "Interp" >:::
+                   [
+                     "add" >:: ( fun () ->
+                         let add = [| Mov(0, 2); MovImm(1, 1); Add(2, 1); Halt|] in
+                         let reg = [|10; 0; 0; 0|] in
+                         let mem = [||] in
+                         let flag = Array.make 256 0 in
+                         let res = interp add reg mem flag 0 in
+                         assert_equal 11 (res.(1))
+                       );
 
-let add_spec =
-  Printf.printf "\n=== add spec ===\n";
-  let add = [| Add (0, 1); Halt |] in
-  let res = interp add [|1; 2; 0; 0|] [||] 0 in
-  print_res res
-;;
+                     "add_imm" >:: ( fun () ->
+                         let add_imm = [| AddImm(2, 1); Halt |] in
+                         let reg = [|0; 1; 0; 0|] in
+                         let mem = [||] in
+                         let flag = Array.make 256 0 in
+                         let res = interp add_imm reg mem flag 0 in
+                         assert_equal 3 (res.(1))
+                       );
 
-let add_imm_spec =
-  Printf.printf "\n=== add_imm spec ===\n";
-  let add_imm = [| AddImm (20, 1); Halt |] in
-  let res = interp add_imm (Array.make 100 0) [||] 0 in
-  print_res res
+                     "mov" >:: ( fun () ->
+                         let mov = [| Mov(0, 1); Halt |] in
+                         let reg = [|100; 0; 0; 0 |] in
+                         let mem = [||] in
+                         let flag = Array.make 256 0 in
+                         let res = interp mov reg mem flag 0 in
+                         assert_equal 100 (res.(1));
+                       );
 
-let mov_spec =
-  Printf.printf "\n=== mov spec ===\n";
-  let mov = [| Mov(0, 1);  Halt|] in
-  let reg = Array.make 256 0 in
-  reg.(0) <- 100;
-  let res = interp mov reg [||] 0 in
-  print_res res
-;;
+                     "sum_n" >:: ( fun () ->
+                         let sum_n = [| MovImm(0, 1); MovImm(0, 2); Add(2, 1); AddImm(1, 2); Cmp(0, 2); Jne(2); Add(2, 1); Halt |] in
+                         let reg = [|10; 0; 0; 0; 0|] in
+                         let flag = Array.make 256 0 in
+                         let mem = Array.make 256 0 in
+                         let res = interp sum_n reg mem flag 0 in
+                         assert_equal 55 (res.(1))
+                       );
 
-let sum_n_spec =
-  Printf.printf "\n=== sum_n spec ===\n";
-;;
+                   ]
 
-let () =
-  add_spec;
-  add_imm_spec;
-  mov_spec;
-;;
+(* Test Runner; -verbose:true gives info on succ tests *)
+let _ = run_test_tt test_fixture
+
+(* Compile with
+ * ocamlfind ocamlc -o interpSpec -package outni -linkpkg -g interp.ml interpSpec.ml
+*)
