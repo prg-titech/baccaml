8e64cf603e7a98b7f6ebcba1f02108c8b3a45786
Yusuke Izawa <yuizalp@gmail.com>
2017-10-05 01:03:32 +0900

    WIP: update interp.ml

--- interp.ml
+++ interp.ml
@@ -11,8 +11,6 @@ module ListUtil = struct
     List.fold_left f ([],[]) (List.rev lst)
 end
 
-type instruction = Asm.t
-
 let int_of_id_t (id : Id.t) : int =
   int_of_string (List.nth (Str.split (Str.regexp_string ".") id) 1)
 
@@ -20,10 +18,18 @@ let int_of_id_or_imm = function
   | V (id_t) -> int_of_id_t id_t
   | C (n) -> n
 
-let rec lookup (program : prog) (name : Id.l) : fundef  =
-  match program with
+let rec lookup (prog : prog) (name : Id.l) : fundef  =
+  match prog with
   | Prog (_, fundefs, _) ->
     List.find (fun (fundef) -> fundef.name = name) fundefs
+
+let rec lookup_by_id_t (prog : prog) (name : Id.t) : fundef =
+  match prog with
+  | Prog (_, fundefs, _) ->
+    let get_fundef_name = function
+      | Id.L (s) -> s
+    in
+    List.find (fun (fundef) -> (get_fundef_name fundef.name) = name) fundefs
 
 (* 仮引数のレジスタに実引数がしまわれている reg_set を作る *)
 let make_reg_set (reg_set : int array) (args_tmp : Id.t list) (args_real : Id.t list) : int array =
@@ -36,7 +42,7 @@ let make_reg_set (reg_set : int array) (args_tmp : Id.t list) (args_real : Id.t 
   set (ListUtil.zip regs_tmp regs_real);
   reg_set
 
-let rec interp (program : prog) (instruction : instruction) (reg_set : int array) (mem : int array): 'a =
+let rec interp (program : prog) (instruction : Asm.t) (reg_set : int array) (mem : int array) : 'a =
   match instruction with
   | Ans exp -> interp_exp program exp reg_set mem
   | Let ((id, _), exp, body) ->
@@ -67,13 +73,55 @@ and interp_exp (program : prog) (exp' : exp) (reg_set : int array) (mem : int ar
     let x' = int_of_id_t x in
     let y' = int_of_id_t x in
     reg_set.(y') - reg_set.(x')
-  | IfEq (id1, V (id2), t1, t2) ->
+  | FMulD (x, y) ->
+    let x' = int_of_id_t x in
+    let y' = int_of_id_t x in
+    reg_set.(y') * reg_set.(x')
+  | FDivD (x, y) ->
+    let x' = int_of_id_t x in
+    let y' = int_of_id_t x in
+    reg_set.(y') / reg_set.(x')
+  | IfEq (id1, id_or_imm, t1, t2) ->
+    let r1 = reg_set.(int_of_id_t id1) in
+    let r2 = reg_set.(int_of_id_or_imm id_or_imm) in
+    if r1 = r2 then
+      interp program t1 reg_set mem
+    else
+      interp program t2 reg_set mem
+  | IfLE (id, id_or_imm, t1, t2) ->
+    let r1 = reg_set.(int_of_id_t id) in
+    let r2 = reg_set.(int_of_id_or_imm id_or_imm) in
+    if r1 <= r2 then
+      interp program t1 reg_set mem
+    else
+      interp program t2 reg_set mem
+  | IfGE (id, id_or_imm, t1, t2) ->
+    let r1 = reg_set.(int_of_id_t id) in
+    let r2 = reg_set.(int_of_id_or_imm id_or_imm) in
+    if r1 >= r2 then
+      interp program t1 reg_set mem
+    else
+      interp program t2 reg_set mem
+  | IfFEq (id1, id2, t1, t2) ->
     let r1 = reg_set.(int_of_id_t id1) in
     let r2 = reg_set.(int_of_id_t id2) in
     if r1 = r2 then
       interp program t1 reg_set mem
     else
       interp program t2 reg_set mem
+  | IfFLE (id1, id2, t1, t2) ->
+    let r1 = reg_set.(int_of_id_t id1) in
+    let r2 = reg_set.(int_of_id_t id2) in
+    if r1 <= r2 then
+      interp program t1 reg_set mem
+    else
+      interp program t2 reg_set mem
+  | CallCls (name, args, _) ->
+    let fundef = lookup_by_id_t program name in
+    let args' = fundef.args in
+    let body' = fundef.body in
+    let reg_set' = make_reg_set reg_set args args' in
+    interp program body' reg_set' mem
   | CallDir (name, args, _) ->
     let fundef = lookup program name in
     (* 仮引数: args' 実引数: args *)
