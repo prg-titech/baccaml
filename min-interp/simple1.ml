8be67daf37a8e6773d752e9f02237ef3cf014682
Yusuke Izawa <yuizalp@gmail.com>
2017-11-14 21:11:24 +0900

    Add sumple interpreter and its' intermediate code

---
+++ min-interp/simple1.ml
@@ -0,0 +1,25 @@
+let rec interpret bytecode pc a =
+  let opcode = bytecode.(pc) in
+  if opcode = 0 then (* INCR_A *)
+    interpret bytecode (pc + 1) (a + 1)
+  else if opcode = 1 then (* DECR_A *)
+    interpret bytecode (pc + 1) (a - 1)
+  else if opcode = 2 then (* JUMP_IF *)
+    let target = bytecode.(pc + 1) in
+    if a > 0 then
+      interpret bytecode target a
+    else
+      interpret bytecode (pc + 2) a
+  else if opcode = 3 then (* JUMP *)
+    let target = bytecode.(pc + 1) in
+    interpret bytecode target a
+  else if opcode = 4 then (* RETURN_A *)
+    a
+  else
+    -1
+in
+let input = Array.make 100 0 in
+input.(0) <- 1;
+input.(1) <- 2; input.(2) <- 0;
+input.(3) <- 4;
+print_int (interpret input 0 100)
