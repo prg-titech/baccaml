e3db12ee8c5cf5fe36a6dd752a6d5c563271b04e
Yusuke Izawa <yuizalp@gmail.com>
2017-11-20 18:03:46 +0900

    Update jit.ml

--- jit.ml
+++ jit.ml
@@ -6,9 +6,6 @@ exception Un_supported of string
 type value = Red of int
            | Green of int
 
-type register = value array
-type memory = value array
-
 let int_of_id_t = function (* TODO: レジスタ番号をsringで与える実装に変更 *)
   | "min_caml_hp" -> raise (Un_supported ("int_of_id_t min_caml_hp is not supported."))
   | id ->
@@ -21,13 +18,22 @@ let int_of_id_or_imm = function
   | V (id_t) -> int_of_id_t id_t
   | C (n) -> n
 
-let get_variable_name str =
+let name_of_id_t str =
   List.hd (Str.split (Str.regexp_string ".") str)
 
-let rec jit (prog : prog) (instr : t) (reg : register) (mem : memory) (reds : string list) (greens : string list): t =
+let is_green id_t greens =
+  List.mem (name_of_id_t id_t) greens
+
+let rec jit
+          (prog : prog)
+          (instr : t)
+          (reg : value array)
+          (mem : value array)
+          (reds : string list)
+          (greens : string list) : t =
   match instr with
   | Let ((dest, typ), Add (id_t1, id_or_imm), body) ->
-     if List.mem (get_variable_name id_t1) greens then
+     if (is_green id_t1 greens) then
        let res = match reg.(int_of_id_t id_t1) with
          | Red (i) -> i + (int_of_id_or_imm id_or_imm)
          | Green (i) -> i + (int_of_id_or_imm id_or_imm)
