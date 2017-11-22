fb6bb841103c1d9be5c372b2750ad5eafb18d2e6
Yusuke Izawa <yuizalp@gmail.com>
2017-11-22 19:48:06 +0900

    Update jitTest.ml

--- jitTest.ml
+++ jitTest.ml
@@ -19,6 +19,22 @@ let _ = run_test_tt_main begin
         assert_equal (value_of reg.(81)) 2;
         assert_equal res (Let (("Ti22.83", Type.Int), Add ("a.42", C (1)),
                                Ans (Mov ("a.42"))))
+      end;
+      "test 2" >::
+      begin fun () ->
+        let instr =
+          Let (("Ti25.77", Type.Int), Add ("pc.41", C (1)),
+               Let (("Ti27.39", Type.Int), Sub ("a.42", C (1)),
+                    Ans (Mov ("a.42"))))
+        in
+        let reg = Array.make 100 (Red (0)) in
+        let mem = Array.make 100 (Red (0)) in
+        reg.(41) <- Green (1);
+        reg.(42) <- Red (100);
+        let res = jitcompile instr reg mem in
+        assert_equal (value_of reg.(77)) 2;
+        assert_equal res (Let (("Ti27.39", Type.Int), Sub ("a.42", C (1)),
+                               Ans (Mov ("a.42"))))
       end
     ]
   end
