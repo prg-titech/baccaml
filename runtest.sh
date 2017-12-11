b13aabb66cbf6b33628af215213b599deba84c43
Yusuke Izawa <yuizalp@gmail.com>
2017-12-12 02:23:30 +0900

    add runtest.sh

---
+++ runtest.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+eval "$(opam config env)"
+
+for OPT in "$@"
+do
+  case "$OPT" in
+    '-a' | '--all' )
+      make test
+      exit 0
+      ;;
+    '-p' | '--pypy' )
+      make pypytest
+      exit 0
+      ;;
+    '-s' | '--simple' )
+      make simple1test
+      exit 0
+      ;; 
+    * )
+      if [[ ! -z "$1" ]] && [[ ! "$1" =~ ^-+ ]]; then
+        param+=( "$1" )
+        shift 1
+      fi
+      ;;
+  esac
+done
+
+if [ -z "$param" ]; then
+  make test
+fi
