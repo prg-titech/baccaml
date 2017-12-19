a7818dc236930475bb291937b34caeedfa35cfef
Yusuke Izawa <yuizalp@gmail.com>
2017-12-20 00:25:54 +0900

    not use ounit for simple1Test

--- test/simple1Test.ml
+++ test/simple1Test.ml
@@ -1,4 +1,3 @@
-open OUnit
 open Asm
 open Util
 open Jit
@@ -6,49 +5,50 @@ open JitUtil
 open MincamlUtil
 open TestUtil
 
-let file = dir ^ "simple1"
+let prog =
+  open_in (dir ^ "simple1.ml")
+  |> Lexing.from_channel
+  |> virtualize
 
-let _ = run_test_tt_main begin
-    "tracing_jit_test" >::: [
-      "simple1_test" >::
-      begin fun () ->
-        let prog =
-          open_in (file ^ ".ml")
-          |> Lexing.from_channel
-          |> virtualize
-        in
-        let Prog (_, fundefs, main) = prog in
-        let fundef = match fundefs with
-          | [fundef] -> fundef
-          | _ -> failwith "Error."
-        in
-        let instr = fundef.body in
-        let reg = Array.make 1000 (Red 0) in
-        let mem = Array.make 1000 (Red 0) in
-        mem.(0 * 4) <- Green (1);
-        mem.(1 * 4) <- Green (2);
-        mem.(2 * 4) <- Green (0);
-        mem.(3 * 4) <- Green (4);
-        reg.(40) <- Green (0);
-        reg.(41) <- Green (0);
-        reg.(42) <- Red (100);
-        let jit_args =
-          { trace_name = "test_trace.1000"
-          ; reds = ["a.42"]
-          ; greens = []
-          ; loop_header = 0
-          ; loop_pc = 41 }
-        in
-        let res = exec_jitcompile prog instr reg mem jit_args in
-        let prog' = Prog ([], fundef :: res :: [], main) in
-        let reg' = Array.make 10000 0 in
-        let mem' = Array.make 10000 0 in
-        JitUtil.enable_jit := true;
-        Logger.log_level := Logger.Debug;
-        EmitVirtual.to_string_fundef res |> print_string;
-        (* ignore (Interp.interp (Interp.to_prog_with_label prog') main reg' mem' jit_args); *)
-        (* prog' |> Simm.f |> RegAlloc.f |> Emit.f (open_out ("test/simple1.s")); *)
-        ()
-      end;
-    ]
-  end
+let Prog (_, fundefs, main) = prog
+
+let fundef = List.hd fundefs
+
+let prepare reg mem =
+  mem.(0 * 4) <- Green (1);
+  mem.(1 * 4) <- Green (2);
+  mem.(2 * 4) <- Green (0);
+  mem.(3 * 4) <- Green (4);
+  reg.(40) <- Green (0);
+  reg.(41) <- Green (0);
+  reg.(42) <- Red (100);
+  ()
+
+let jit_args =
+  { trace_name = "test_trace.1000"
+  ; reds = ["a.42"]
+  ; greens = []
+  ; loop_header = 0
+  ; loop_pc = 41 }
+
+let _ =
+  Arg.parse
+    [("-jit", Arg.Unit (fun _ -> JitUtil.enable_jit := true), "enable jit compile");
+     ("-debug", Arg.Unit (fun _ -> Logger.log_level := Logger.Debug), "debug mode");]
+    (fun s -> ())
+    ("usage: -jit: enable jit, -debug: execute as debug mode");
+  let instr = fundef.body in
+  let reg, mem = Array.make 1000 (Red 0), Array.make 1000 (Red 0) in
+  let reg', mem' = Array.make 10000 0, Array.make 10000 0 in
+  prepare reg mem;
+  let res = exec_jitcompile prog instr reg mem jit_args in
+  let prog' = Prog ([], fundef :: res :: [], main) in
+  setup reg reg';
+  setup mem mem';
+  print_string (EmitVirtual.to_string_fundef res);
+  ignore(
+    Interp.interp (Interp.to_prog_with_label prog') main reg' mem' jit_args
+  );
+  prog' |> Simm.f |> RegAlloc.f |> Emit.f (open_out ("test/simple1.s"));
+  ()
+
