9ee5879e34869d9322f41dfe94a238de62469ee1
Yusuke Izawa <yuizalp@gmail.com>
2017-12-25 21:29:48 +0900

    fix methodJit.ml

--- src/jit/methodJit.ml
+++ src/jit/methodJit.ml
@@ -31,13 +31,7 @@ and method_jit_ans p e reg mem jit_args =
   match e with
   | CallDir (id_l, argsr, _) ->
     let fundef = find_fundef p id_l in
-    let pc = value_of reg.(Util.find_pc argsr 1) in
-    (match pc = (jit_args.loop_header) with
-     | true ->
-       let reds = List.filter ~f:(fun a -> is_red reg.(int_of_id_t a)) argsr in
-       Ans (CallDir (Id.L (jit_args.trace_name), reds, []))
-     | false ->
-       method_jit p (inline_calldir_exp argsr fundef reg) reg mem jit_args)
+    method_jit p (inline_calldir_exp argsr fundef reg) reg mem jit_args
   | IfEq (id_t, id_or_imm, t1, t2) ->
     let r2 = Util.value_of_id_or_imm reg id_or_imm in
     Ans (match r2 with
