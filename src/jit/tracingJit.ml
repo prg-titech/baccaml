9e3f94c6bff84e0ca453846c262f04353069d360
Yusuke Izawa <yuizalp@gmail.com>
2017-12-25 21:16:36 +0900

    extract inlining module

--- src/jit/tracingJit.ml
+++ src/jit/tracingJit.ml
@@ -2,41 +2,9 @@ open Asm
 open Core
 open JitConfig
 open Renaming
+open Inlining
 
 exception Not_supported of string
-
-module Inline = struct
-
-  let rec inline_args argsr argst funbody reg =
-    match argsr, argst with
-    | [], [] ->
-      funbody
-    | hdr :: tlr, hdt :: tlt when hdr = hdt ->
-      inline_args tlr tlt funbody reg
-    | hdr :: tlr, hdt :: tlt ->
-      reg.(int_of_id_t hdt) <- reg.(int_of_id_t hdr);
-      Let ((hdt, Type.Int), Mov (hdr), (inline_args tlr tlt funbody reg))
-    | _ ->
-      failwith "Un matched pattern."
-
-  let rec inline_calldir argsr dest fundef contbody reg =
-    let { args; body } = Renaming.rename_fundef fundef in
-    let rec add_cont_proc id_t instr =
-      match instr with
-      | Let (a, e, t) ->
-        Let (a, e, add_cont_proc id_t t)
-      | Ans e ->
-        Let ((id_t, Type.Int), e, contbody)
-    in
-    add_cont_proc dest (inline_args argsr args body reg)
-
-  let rec inline_calldir_exp argsr fundef reg =
-    let { args; body } = Renaming.rename_fundef fundef in
-    inline_args argsr args body reg
-
-end
-
-open Inline
 
 module Guard = struct
   let select_branch e n1 n2 t1 t2 =
