e9f49df7e79f3e52f8a198aec96e9ecfd72c6ecf
Yusuke Izawa <yuizalp@gmail.com>
2017-12-27 00:42:03 +0900

    add jitInterop.mli

---
+++ src/jit/jitInterop.mli
@@ -0,0 +1,9 @@
+val find_red_in_body : Asm.t -> Core.String.t Core.List.t -> Id.t Core.List.t
+val find_red_in_fundefs :
+  Asm.fundef Core.List.t -> Core.String.t Core.List.t -> Id.t Core.List.t
+val find_red_registers :
+  Asm.prog -> Core.String.t Core.List.t -> Id.t Core.List.t
+val convert :
+  Asm.prog ->
+  int Core.Array.t ->
+  Core.String.t Core.List.t -> JitConfig.value Core.Array.t
