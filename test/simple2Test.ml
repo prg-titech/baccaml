7ff5e99ec2d8fe68c70e34acaea16516fce1c832
Yusuke Izawa <yuizalp@gmail.com>
2018-01-25 00:56:25 +0900

    update simpleTest2

--- test/simple2Test.ml
+++ test/simple2Test.ml
@@ -8,20 +8,22 @@ open JitConfig
 open MincamlUtil
 open TestUtil
 
+let Prog (_, fundefs, main) as prog =
+  In_channel.create (dir ^ "simple2.ml")
+  |> Lexing.from_channel
+  |> virtualize
+
+let fundef = match List.hd fundefs with
+    Some v -> v
+  | None -> failwith "List.hd is failed."
+
+let { body; } = fundef
+
+let bytecode = [|0; 11; 4; 8; 0; 0; 10; 12; 1; 1; 10; 12; 20|]
+
 let _ = run_test_tt_main begin
-    "method_jit_test" >::: [
-      "simple2_test" >::
-      begin fun () ->
-        let prog =
-          In_channel.create (dir ^ "simple2.ml")
-          |> Lexing.from_channel
-          |> virtualize
-        in
-        let Prog (_, fundefs, main) = prog in
-        let fundef = match List.hd fundefs with
-            Some v -> v
-          | None -> failwith "List.hd is failed."
-        in
+    "simple2_test" >::: [
+      "method_jit" >:: begin fun () ->
         let method_jit_args = {
           method_name = "test_method.1000";
           reds = ["a.69"];
@@ -29,10 +31,8 @@ let _ = run_test_tt_main begin
           method_end = 12;
           pc_place = 1
         } in
-        let { body; } = fundef in
         let reg = Array.create 10000 (Red (0)) in
         let mem = Array.create 10000 (Red (0)) in
-        let bytecode = [|0; 11; 4; 8; 0; 0; 10; 12; 1; 1; 10; 12; 20|] in
         for i = 0 to (Array.length bytecode - 1) do
           mem.(0 + i * 4) <- Green (bytecode.(i))
         done;
@@ -42,5 +42,25 @@ let _ = run_test_tt_main begin
         print_string (EmitVirtual.to_string_fundef res);
         ()
       end;
+      "tracing_jit" >:: begin fun () ->
+        let tracing_jit_args = {
+          trace_name = "test_trace.1000";
+          reds = ["a.69"];
+          greens = [];
+          loop_header = 0;
+          loop_pc_place = 1
+        } in
+        let reg = Array.create 100000 (Red (0)) in
+        let mem = Array.create 100000 (Red (0)) in
+        reg.(67) <- Green (0);
+        reg.(68) <- Green (0);
+        for i = 0 to (Array.length bytecode - 1) do
+          mem.(0 + i * 4) <- Green (bytecode.(i))
+        done;
+        let res = TracingJit.exec_tracing_jit prog body reg mem tracing_jit_args in
+        print_string (EmitVirtual.to_string_fundef res);
+
+        ()
+      end
     ]
   end
