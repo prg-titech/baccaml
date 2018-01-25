c01ebb0a4c567f6e433597bf9a290a34186b23c1
Yusuke Izawa <yuizalp@gmail.com>
2018-01-26 02:47:46 +0900

    wip

--- test/simple3Test.ml
+++ test/simple3Test.ml
@@ -20,22 +20,23 @@ let _ = run_test_tt_main begin
         let fundef = List.hd_exn fundefs in
         let method_jit_args = {
           method_name = "test_method.1000";
-          reds = [];
+          reds = ["stack.112"; "sp.113"];
           method_start = 0;
-          method_end = 0;
-          pc_place = 2
+          method_end = 6;
+          pc_place = 1
         } in
         let { body } = fundef in
-        let reg = Array.create 100000 (Red 0) in
-        let mem = Array.create 100000 (Red 0) in
-        reg.(110) <- Red (0);
-        reg.(111) <- Green (7);
+        let reg = Array.create 10000 (Red 0) in
+        let mem = Array.create 10000 (Red 0) in
+        reg.(110) <- Green (0);
+        reg.(111) <- Green (0);
         reg.(112) <- Red (100);
-        reg.(113) <- Green (1);
+        reg.(113) <- Red (1);
         let bytecode = [|4; 5; 22; 2; 22; 2; 0; 11; 2; 10; 0; 30|] in
         for i = 0 to (Array.length bytecode - 1) do
           mem.(i * 4) <- Green (bytecode.(i))
         done;
+        mem.(100) <- Green (4); mem.(104) <- Green (5);
         let res = method_jit prog body reg mem method_jit_args in
         print_string (EmitVirtual.to_string_t res);
         ()
