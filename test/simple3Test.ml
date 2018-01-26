2bbb53604e88ca01719d5ba4489aa97b0f5b5020
Yusuke Izawa <yuizalp@gmail.com>
2018-01-26 19:54:52 +0900

    fix simple3Test

--- test/simple3Test.ml
+++ test/simple3Test.ml
@@ -31,11 +31,13 @@ let _ = run_test_tt_main begin
         let mem = Array.create 10000 (Red 0) in
         reg.(116) <- Green (0);
         reg.(117) <- Green (6);
-        reg.(1118) <- Red (100);
-        reg.(119) <- Red (0);
+        reg.(118) <- Green (100);
+        reg.(119) <- Green (0);
         for i = 0 to (Array.length bytecode - 1) do
           mem.(i * 4) <- Green (bytecode.(i))
         done;
+        mem.(11 * 4) <- Red (4);
+        mem.(12 * 4) <- Red (5);
         let res = method_jit prog body reg mem method_jit_args in
         print_string (EmitVirtual.to_string_t res);
         print_newline ();
@@ -55,11 +57,13 @@ let _ = run_test_tt_main begin
         let mem = Array.create 10000 (Red 0) in
         reg.(116) <- Green (0);
         reg.(117) <- Green (6);
-        reg.(118) <- Red (100);
-        reg.(119) <- Red (0);
+        reg.(118) <- Green (100);
+        reg.(119) <- Green (0);
         for i = 0 to (Array.length bytecode - 1) do
           mem.(i * 4) <- Green (bytecode.(i))
         done;
+        mem.(11 * 4) <- Red (4);
+        mem.(12 * 4) <- Red (5);
         let res = TracingJit.tracing_jit prog body reg mem tracing_jit_args in
         print_string (EmitVirtual.to_string_t res);
       end
