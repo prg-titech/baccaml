8d1ba97f0e0e0a21178e0d9a42ec20b2a5484895
Yusuke Izawa <yuizalp@gmail.com>
2018-01-26 15:17:48 +0900

    update simple3

--- data/simple3.ml
+++ data/simple3.ml
@@ -1,4 +1,16 @@
+(*
+let print_stack st =
+  let l = Array.to_list st in
+  let rec print_list = function
+      [] ->
+      ()
+    | e :: l ->
+      print_int e ; print_string " " ; print_list l
+  in print_list l
+in
+*)
 let rec interp bytecode pc stack sp =
+  (* print_stack stack; print_newline (); *)
   let instr = bytecode.(pc) in
   if instr = 0 then (* Add *)
     let v2 = stack.(sp) in
@@ -14,16 +26,18 @@ let rec interp bytecode pc stack sp =
     let n = bytecode.(pc + 1) in
     stack.(sp) <- n;
     interp bytecode (pc + 2) stack (sp + 1)
-  else if instr = 10 then (* CALL *)
+  else if instr = 10 then (* CALL addr n1 n2 *)
     let addr = bytecode.(pc + 1) in
-    stack.(sp + 1) <- (pc + 2);
-    interp bytecode addr stack (sp + 1)
+    let n1 = bytecode.(pc + 2) in
+    let n2 = bytecode.(pc + 3) in
+    stack.(sp) <- n2;
+    stack.(sp + 1) <- n1;
+    let v = interp bytecode addr stack (sp + 2) in
+    stack.(sp) <- v;
+    interp bytecode (pc + 4) stack (sp)
   else if instr = 11 then (* RET *)
-    let n = bytecode.(pc + 1) in
     let v = stack.(sp) in
-    let raddr = stack.(sp - 1) in
-    stack.(sp - n - 1) <- v;
-    interp bytecode raddr stack (sp - n - 1)
+    v
   else if instr = 22 then (* DUP *)
     let n = bytecode.(pc + 1) in
     let v = stack.(sp - n) in
@@ -35,12 +49,11 @@ let rec interp bytecode pc stack sp =
     -1
 in
 let code = Array.make 100 0 in
-let stack = Array.make 100 0 in
-stack.(0) <- 4; stack.(1) <- 5;
+let stack = Array.make 10 0 in
 code.(0) <- 22; code.(1) <- 2;
 code.(2) <- 22; code.(3) <- 2;
 code.(4) <- 0;
 code.(5) <- 11; code.(6) <- 2;
-code.(7) <- 10; code.(8) <- 0;
-code.(9) <- 30;
-print_int (interp code 7 stack 1)
+code.(7) <- 10; code.(8) <- 0; code.(9) <- 4; code.(10) <- 5;
+code.(11) <- 30;
+print_int (interp code 7 stack 0)
