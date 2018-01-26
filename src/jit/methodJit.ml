6f6318489e2bffca639cd90e87378a714d5a0ff6
Yusuke Izawa <yuizalp@gmail.com>
2018-01-26 15:17:57 +0900

    update methodJIT

--- src/jit/methodJit.ml
+++ src/jit/methodJit.ml
@@ -41,6 +41,25 @@ let rec method_jit p instr reg mem method_jit_args =
   match instr with
   | Ans (exp) ->
     method_jit_ans p exp reg mem method_jit_args
+  | Let ((dest, typ), CallDir (id_l, args, fargs), body) ->
+    begin
+      let rec go cont = function
+      [] -> cont
+    | hd :: tl ->
+      if is_green reg.(int_of_id_t hd) then
+        Let ((hd, Type.Int),
+             Set (value_of reg.(int_of_id_t hd)),
+             go cont tl)
+      else
+        go cont tl
+      in
+      let t =
+        Let ((dest, typ),
+             CallDir (id_l, args, fargs),
+             (method_jit p body reg mem method_jit_args))
+      in
+      go t args
+      end
   | Let ((dest, typ), exp, body) ->
     begin
       match TracingJit.tracing_jit_let p exp reg mem with
@@ -49,7 +68,9 @@ let rec method_jit p instr reg mem method_jit_args =
         method_jit p body reg mem method_jit_args
       | Not_specialized (e, v) ->
         reg.(int_of_id_t dest) <- v;
-        Let ((dest, typ), e, method_jit p body reg mem method_jit_args)
+        Let ((dest, typ),
+             e,
+             method_jit p body reg mem method_jit_args)
     end
 
 and method_jit_ans p e reg mem method_jit_args = match e with
