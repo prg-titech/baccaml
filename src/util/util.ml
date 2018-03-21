2215d21af706160fe88cdf1eafd50e469ec59bd1
Yusuke Izawa <yuizalp@gmail.com>
2018-03-21 19:40:20 +0900

    remove to_string

--- src/util/util.ml
+++ src/util/util.ml
@@ -27,16 +27,6 @@ module ListUtil = struct
     print_elements lst;
     print_string "]"
 
-  let to_string s =
-    let rec loop s n =
-      match s with
-        [] -> String.make n '?'
-      | car :: cdr ->
-        let result = loop cdr (n + 1) in
-        Bytes.set result n car;
-        result
-    in
-    loop s 0
 end
 
 module ArrayUtil = struct
