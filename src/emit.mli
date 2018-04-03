2007c9d1df56b6f726f8fce964fd34764318d6fc
Yusuke Izawa <yuizalp@gmail.com>
2018-04-04 00:13:21 +0900

    add new external functions

--- src/emit.mli
+++ src/emit.mli
@@ -1,3 +1,11 @@
+val stackset : S.t ref
+val stackmap : S.elt list ref
+val save : string -> unit
+val savef : S.elt -> unit
+val locate : S.elt -> int list
+val offset : string -> int
+val stacksize : unit -> int
+val pp_id_or_imm : Asm.id_or_imm -> string
 type dest = Tail | NonTail of Id.t
 val g : out_channel -> dest * Asm.t -> unit
 val h : out_channel -> Asm.fundef -> unit
