57d6ff87d10a395d252ab381ac51df4aa52c582e
Yusuke Izawa <yuizalp@gmail.com>
2018-06-02 16:17:34 +0900

    fix main module

--- src/main.ml
+++ src/main.ml
@@ -1,35 +1,24 @@
 open Mincaml
+open Mincaml.Mincaml_util
 open Util
-open Mincaml_util
+
+open Core
 
 let emit_virtual = ref false
 let is_interp = ref false
 let trim = ref false
 
-let compile outchan l =
-  if !trim then
-    virtualize l
-    |> Trim.f
-    |> Simm.f
-    |> RegAlloc.f
-    |> Emit.f outchan
-  else
-    virtualize l
-    |> Simm.f
-    |> RegAlloc.f
-    |> Emit.f outchan
-
 let interp l =
   virtualize l |> Interp.f
 
 let interp_exec f =
-  let inchan = open_in (f ^ ".ml") in
-  try ignore (interp (Lexing.from_channel inchan)); close_in inchan;
+  let inchan = In_channel.create (f ^ ".ml") in
+  try ignore (interp (Lexing.from_channel inchan)); In_channel.close inchan;
   with e ->
-    close_in inchan; raise e
+    In_channel.close inchan; raise e
 
 let dump_exec f =
-  let inchan = open_in (f ^ ".ml") in
+  let inchan = In_channel.create (f ^ ".ml") in
   try
     (if !trim then      
        Lexing.from_channel inchan
@@ -44,23 +33,44 @@ let dump_exec f =
        |> Simm.f
        |> Emit_virtual.to_string_prog
        |> print_endline);
-    close_in inchan;
+    In_channel.close inchan;
   with e ->
-    close_in inchan;
+    In_channel.close inchan;
     raise e
+
+let compile outchan l =
+  if !trim then
+    virtualize l
+    |> Trim.f
+    |> Simm.f
+    |> RegAlloc.f
+    |> Emit.f outchan
+  else
+    virtualize l
+    |> Simm.f
+    |> RegAlloc.f
+    |> Emit.f outchan
 
 (* 文字列をコンパイルして標準出力に表示する (caml2html: main_string) *)
 let string s = compile stdout (Lexing.from_string s)
 
 (* ファイルをコンパイルしてファイルに出力する (caml2html: main_file) *)
 let compile_exec f =
-  let inchan = open_in (f ^ ".ml") in
-  let outchan = open_out (f ^ ".s") in
+  let flst = String.split ~on:'.' f in
+  ignore (match List.last flst with
+  | Some (v) when v = "ml" -> ()
+  | _ -> failwith "No suffix or suffix is not .ml.");
+  let f' = match flst |> List.hd with
+    | Some (v) -> v
+    | None -> failwith "No suffix. please add .ml"
+  in
+  let inchan = In_channel.create (f' ^ ".ml") in
+  let outchan = Out_channel.create (f' ^ ".s") in
   try
     compile outchan (Lexing.from_channel inchan);
-    close_in inchan;
-    close_out outchan;
-  with e -> (close_in inchan; close_out outchan; raise e)
+    In_channel.close inchan;
+    Out_channel.close outchan;
+  with e -> (In_channel.close inchan; Out_channel.close outchan; raise e)
 
 let () = (* ここからコンパイラの実行が開始される (caml2html: main_entry) *)
   let files = ref [] in
@@ -79,7 +89,7 @@ let () = (* ここからコンパイラの実行が開始される (caml2html: m
      exit 1)
   else
     List.iter
-      begin fun f ->
+      ~f:begin fun f ->
         if !is_interp then ignore (interp_exec f)
         else if !emit_virtual then ignore (dump_exec f)
         else ignore (compile_exec f)
