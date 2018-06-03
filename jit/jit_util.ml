80c53d2443e54abdf3a26d0a20f583394da0e3b6
Yusuke Izawa <yuizalp@gmail.com>
2018-06-03 20:28:20 +0900

    fix jit_util

--- jit/jit_util.ml
+++ jit/jit_util.ml
@@ -6,7 +6,9 @@ let int_of_id_t id =
   if String.equal id "min_caml_hp" then
     failwith ("int_of_id_t min_caml_hp is not supported.")
   else
-    try
-      int_of_string (StringUtil.after_of id '.')
-    with _ ->
-      int_of_string (StringUtil.after_of id 'u')
+    match int_of_string_opt (StringUtil.after_of id '.') with
+    | Some (i) -> i
+    | _ ->
+      match int_of_string_opt (StringUtil.after_of id 'u') with
+      | Some (i) -> i
+      | _ -> failwith (Printf.sprintf "int_of_id_t (%s) is failed" id)      
