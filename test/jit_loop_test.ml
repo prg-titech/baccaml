dab926d467c97694fbce08689b8b952547aa38a2
Yusuke Izawa <yuizalp@gmail.com>
2018-07-01 22:57:45 +0900

    fix jit_loop_test

--- test/jit_loop_test.ml
+++ test/jit_loop_test.ml
@@ -20,24 +20,17 @@ let _ = run_test_tt_main begin
       "test1" >::
       begin fun () ->
         Logger.log_level := Logger.Debug;
-        let t =
-          Let ((Id.gentmp Type.Unit, Type.Unit), CallDir (Id.L "min_caml_loop_start", [], []),
-                     Let (("x.10", Type.Int), Add ("a.9", V ("b.8")),
-                          Let (("ans.11", Type.Int), Add ("x.10", V ("x.10")),
-                               Let ((Id.gentmp Type.Unit, Type.Unit), CallDir (Id.L "min_caml_loop_end", [], []),
-                                    Let (("y.12", Type.Int), Sub ("ans.11", C (100)),
-                                         Ans (Mov ("y.12"))))))) in
-        let p = Prog ([], [], Ans (Nop)) in
-        let reg = Array.make 100 (Red (0)) in
-        let mem = Array.make 100 (Red (0)) in
-        let res = Method_jit_loop.find_loop_start_pc p reg mem t in
-        print_list print_int res
-      end;
-      "test2" >::
-      begin fun () ->
-        Logger.log_level := Logger.Debug;
         (* preprocesses *)
-        let bytecode = [|1; 1; 7; 1; 10; 4; 2; 13; 6; 0; 5; 3; 5; 7 |] in
+        let bytecode =
+          [|1; 1;
+            7;
+            1; 10;
+            4;
+            2; 13;
+            6; 0;
+            5;
+            3; 5;
+            7 |] in
         let p =
           open_in ((Sys.getcwd ()) ^ "/test/jit_loop.ml")
           |> Lexing.from_channel
@@ -63,15 +56,15 @@ let _ = run_test_tt_main begin
         let res = match Method_jit.exec p (fundef'.body) reg mem () with
           | Method_success fundef | Tracing_success fundef -> fundef
         in
-        print_endline "[RESULT]" |> fun () -> Asm.show_fundef res |> print_endline;
+        print_endline "[RESULT]" |> fun () -> Emit_virtual.to_string_fundef res |> print_endline;
 
         (* extract loop function *)
         let loop = Method_jit.find_loop "test_loop_fun" res.body in
-        print_endline "[LOOP FUNCTION]" |> fun () -> Asm.show_fundef loop |> print_endline;
+        print_endline "[LOOP FUNCTION]" |> fun () -> Emit_virtual.to_string_fundef loop |> print_endline;
         
         (* extract non loop function *)
         let nonloop = Method_jit.find_nonloop "test_loop_fun" res in
-        print_endline "[NONLOOP FUNCTION]" |> fun () -> Asm.show_fundef nonloop |> print_endline;
+        print_endline "[NONLOOP FUNCTION]" |> fun () -> Emit_virtual.to_string_fundef nonloop |> print_endline;
         
         (* Jit_emit.emit_trace (Method_success (nonloop)) "nonloop" "interp.88";
          * Jit_emit.emit_trace (Method_success (loop)) "loop" "interp.88"; *)
