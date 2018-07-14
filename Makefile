049b590f1a76556ccbf7eded464eeebfe4a8c960
Yusuke Izawa <yuizalp@gmail.com>
2018-07-14 23:09:36 +0900

    fixup

--- Makefile
+++ Makefile
@@ -1,67 +1,56 @@
 .default: build
 
-.all: build clean jit-clean example test test-one gcc
+all: build test
+
+-include: Makefile.mincaml
 
 CC = gcc
 CFLAGS = -g -O2 -Wall
 OCAMLLDFLAGS = -warn-error -31
 
 MAIN = main.exe
+ARMIN = armin.exe
 COMPILER = min-caml
-
-EXAMPLES = print sum-tail gcd sum fib ack even-odd adder \
-funcomp cls-rec cls-bug cls-bug2 cls-reg-bug shuffle \
-spill spill2 spill3 join-stack join-stack2 join-stack3 \
-join-reg join-reg2 non-tail-if non-tail-if2 inprod inprod-rec \
-inprod-loop matmul matmul-flat manyargs fib-tail array array2 \
-float tuple
 
 .PHONY: build
 build:
-	@jbuilder build
-	@ln -sf _build/default/src/mincaml/$(MAIN) .
-	@ln -sf _build/default/src/armin/armin.exe .
-	@mv $(MAIN) $(COMPILER)
+	jbuilder build
+	ln -sf _build/default/src/mincaml/$(MAIN) .
+	ln -sf _build/default/src/armin/$(ARMIN) .
+	mv $(MAIN) $(COMPILER)
 
 .PHONY: setup
 setup:
-	@opam install -y merlin ocp-indent tuareg utop core jbuilder ppx_deriving
+	opam install -y merlin ocp-indent tuareg utop core jbuilder ppx_deriving logs fmt
 
 .PHONY: clean
 clean:
-	@jbuilder clean
+	jbuilder clean
 	@rm -rf $(TRASH)
 
 .PHONY: clean-jit
 clean-jit:
-	@rm -rf *.o *.s test/*.o test/*.s *.dSYM
-	@rm -rf simple*_*j
-
-.PHONY: clean-dump
-clean-dump:
-	@rm -rf **/*.dump
+	rm -rf *.o *.s test/*.o test/*.s *.dSYM
+	rm -rf simple*_*j
 
 .PHONY: clean-all
-clean-all: clean clean-jit clean-dump
+clean-all: clean clean-jit
 
 .PHONY: test
 test:
-	@jbuilder runtest
+	jbuilder runtest -f
 
 .PHONY: test-one
 test-one:
-	@jbuilder build test/$(SPEC).exe
-	@cd _build/default/test || exit 1 && ./$(SPEC).exe
+	jbuilder build test/$(SPEC).exe
+	cd _build/default/test || exit 1 && ./$(SPEC).exe
 
-TRACE =
-INTERP =
-
-.PHONY: gcc
-gcc:
-	@./$(COMPILER) test/$(INTERP).ml
-	@gcc -c -m32 _build/default/test/$(TRACE).s -o _build/default/test/$(TRACE).o
-	@gcc -g -Wall -O2 -m32 lib/libmincaml.S lib/stub.c test/$(INTERP).s _build/default/test/$(TRACE).o -o $(TRACE)
-	@rm -rf $(TRACE).dSYM
+EXAMPLES = print sum-tail gcd sum fib ack even-odd adder \
+funcomp cls-rec cls-bug cls-bug2 cls-reg-bug shuffle \
+spill spill2 spill3 join-stack join-stack2 join-stack3 \
+join-reg join-reg2 non-tail-if2 inprod inprod-rec \
+inprod-loop matmul matmul-flat manyargs fib-tail array array2 \
+float tuple
 
 .PHONY: example
 example: $(EXAMPLES:%=example/%.cmp)
@@ -69,10 +58,10 @@ example: $(EXAMPLES:%=example/%.cmp)
 .PRECIOUS: example/%.s example/% example/%.res example/%.ans example/%.cmp
 TRASH = $(EXAMPLES:%=example/%.s) $(EXAMPLES:%=example/%) $(EXAMPLES:%=example/%.res) $(EXAMPLES:%=example/%.ans) $(EXAMPLES:%=example/%.cmp)
 
-example/%.s: compiler example/%.ml
-	./$(COMPILER) example/$*
+example/%.s: example/%.ml
+	./$(COMPILER) example/$*.ml
 example/%: example/%.s lib/libmincaml.S lib/stub.c
-	$(CC) $(CFLAGS) -m32 $^ -lm -o $@
+	$(CC) $(CFLAGS) -m32 $^ -o $@
 example/%.res: example/%
 	$< > $@
 example/%.ans: example/%.ml
--- test/virtual_mincaml.ml
+++ test/virtual_mincaml.ml
@@ -1,59 +1,61 @@
-let rec interp bytecode pc stack sp =
+let rec interp bytecode stack pc sp =
+  jit_dispatch (pc=8) bytecode stack;
   let instr = bytecode.(pc) in
   if instr = 0 then             (* ADD *)
-    let v2 = stack.(sp - 1) in  (* sp: sp - 1 *)
-    let v1 = stack.(sp - 2) in  (* sp: sp - 2 *)
-    stack.(sp - 2) <- v1 + v2;  (* sp: sp - 1 *)
-    interp bytecode (pc + 1) stack (sp - 1)
+    let v2 = stack.(sp - 1) in  (* sp - 1 *)
+    let v1 = stack.(sp - 2) in  (* sp - 2 *)
+    stack.(sp - 2) <- v1 + v2;  (* sp - 1 *)
+    interp bytecode stack (pc + 1) (sp - 1)
   else if instr = 1 then        (* SUB *)
     let v2 = stack.(sp - 1) in
     let v1 = stack.(sp - 2) in
     stack.(sp - 2) <- v1 - v2;
-    interp bytecode (pc + 1) stack (sp - 1)
+    interp bytecode stack (pc + 1) (sp - 1)
   else if instr = 3 then        (* LT *)
     let v2 = stack.(sp - 1) in
     let v1 = stack.(sp - 2) in
     stack.(sp - 2) <- (if v1 < v2 then 1 else 0);
-    interp bytecode (pc + 1) stack (sp - 1)
+    interp bytecode stack (pc + 1) (sp - 1)
   else if instr = 4 then        (* CONST *)
     let c = bytecode.(pc + 1) in
     stack.(sp) <- c;
-    interp bytecode (pc + 2) stack (sp + 1)
+    interp bytecode stack (pc + 2) (sp + 1)
   else if instr = 5 then        (* JUMP_IF_ZERO *)
     let addr = bytecode.(pc + 1) in
     let v = stack.(sp - 1) in
     if v = 0 then
-      if addr < pc then
-        (loop_end ();
-         interp bytecode addr stack (sp - 1))
+      if addr < pc then 
+        (loop_end bytecode stack;
+         interp bytecode stack addr (sp - 1))
       else
-        interp bytecode addr stack (sp - 1)
+        interp bytecode stack addr (sp - 1)
     else
-      interp bytecode (pc + 2) stack (sp - 1)
+      interp bytecode stack (pc + 2) (sp - 1)
   else if instr = 6 then        (* CALL *)
     let addr = bytecode.(pc + 1) in
     stack.(sp) <- (pc + 2);
-    interp bytecode addr stack (sp + 1)
+    interp bytecode stack addr (sp + 1)
   else if instr = 7 then        (* RET *)
     let n = bytecode.(pc + 1) in
-    let v = stack.(sp - 1) in   (* sp: sp - 1 *)
-    let pc2 = stack.(sp - 2) in (* sp: sp - 2 *)
-    stack.(sp - n - 2) <- v;    (* sp: sp - 2 - n + 1 = sp - 1 - n *)
-    interp bytecode pc2 stack (sp - n - 1)
+    let v = stack.(sp - 1) in   (* sp - 1 *)
+    let pc2 = stack.(sp - 2) in (* sp - 2 *)
+    stack.(sp - n - 2) <- v;    (* sp - 2 - n + 1 = sp - 1 - n *)
+    interp bytecode stack pc2 (sp - n - 1)
   else if instr = 8 then        (* DUP *)
     let n = bytecode.(pc + 1) in
     let v = stack.(sp - n - 1) in
     stack.(sp) <- v;
-    interp bytecode (pc + 2) stack (sp + 1)
+    interp bytecode stack (pc + 2) (sp + 1)
   else if instr = 9 then        (* HALT *)
     stack.(sp - 1)
   else if instr = 11 then       (* JUMP *)
     let addr = bytecode.(pc + 1) in
-    interp bytecode addr stack sp
+    interp bytecode stack addr sp
   else if instr = 12 then       (* LOOP_S *)
-    interp bytecode (pc + 1) stack sp
+    interp bytecode stack (pc + 1) sp
   else
     -1000 in
+
 let code = Array.make 50 0 in
 let stack = Array.make 50 0 in
 code.(0) <- 4;
@@ -91,4 +93,4 @@ code.(31) <- 5;
 code.(32) <- 0;
 code.(33) <- 7;
 code.(34) <- 1;
-print_int (interp code 0 stack 0)
+print_int (interp code stack 0 0)
