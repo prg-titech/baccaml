8139407d01c8920ca1f8c5a8aa0e88b0f58ad536
Yusuke Izawa <yuizalp@gmail.com>
2018-07-18 02:46:37 +0900

    fixup

--- test/virtual_test.ml
+++ test/virtual_test.ml
@@ -8,7 +8,7 @@ let main name ex_name code annot red_lst green_lst =
   let Env (p, reg, mem, red_args) =
     prepare_env name ex_name code annot red_lst green_lst in
 
-  mem.(100) <- Red (100);
+  mem.(1000) <- Red (1000);
 
   let traces =
     Method_jit_loop.run_while
