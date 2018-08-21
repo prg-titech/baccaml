1188c2790b54218bc85f7a6b9fded2a562af6c3d
Yusuke Izawa <yuizalp@gmail.com>
2018-08-22 06:48:50 +0900

    add rCaml.mli

---
+++ src/bin/rCaml.mli
@@ -0,0 +1,29 @@
+type env =
+  { prog : MinCaml.Asm.prog;
+    reg : BacCaml.Jit_config.value array;
+    mem : BacCaml.Jit_config.value array;
+    red_args : string list;
+    ex_name : string;
+  }
+
+type arg =
+  { file : string;
+    ex_name : string;
+    code : int array;
+    annot : int array;
+    reds : (string * int) list;
+    greens : (string * int) list;
+  }
+
+type var = {
+  redtbl : (string, int) Hashtbl.t;
+  greentbl : (string, int) Hashtbl.t;
+}
+
+type tenv = {
+  fundefs : MinCaml.Asm.fundef list;
+  ibody : MinCaml.Asm.t;
+}
+
+val prepare_env : arg -> env
+val run : (arg -> 'a) -> 'a
