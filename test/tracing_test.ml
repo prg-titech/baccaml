aa3ec2918df9305c5c4426bc22adefe76b8ba7f0
Yusuke Izawa <yuizalp@gmail.com>
2018-08-22 18:05:51 +0900

    add tracing_test

---
+++ test/tracing_test.ml
@@ -0,0 +1,20 @@
+open RCaml
+open MinCaml
+open BacCaml
+open Jit_config
+
+let _ =
+  run begin fun arg ->
+    let { prog; reg; mem; red_args; ex_name } = prepare_env arg in
+
+    let trace =
+      Jit_tracing.run_while
+        prog reg mem
+        "min_caml_test_trace"
+        ("stack" :: red_args)
+        3 0
+    in
+    Logs.debug (fun m -> m "%s" (Emit_virtual.to_string_fundef trace));
+
+    Jit_emit.emit_result_mj ~prog:prog ~traces:[trace] ~file:ex_name
+  end
