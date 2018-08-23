bb517c2218b886c8d78e3154931a57004850a842
Yusuke Izawa <yuizalp@gmail.com>
2018-08-23 23:26:40 +0900

    fix jit_optimizer

--- src/jit/jit_optimizer.ml
+++ src/jit/jit_optimizer.ml
@@ -94,12 +94,11 @@ let run p e reg mem = match e with
   | Ld (id_t, id_or_imm, x) as exp ->
     let destld = reg.(int_of_id_t id_t) in
     let offsetld =
-      (match id_or_imm with
-       | V (id_t) ->
-         (match reg.(int_of_id_t id_t) with
-          | Green (n1) | LightGreen (n1) -> Green (n1 * x)
+      match id_or_imm with
+      | V (id) -> (match reg.(int_of_id_t id) with
+            Green (n1) | LightGreen (n1) -> Green (n1 * x)
           | Red (n1) -> Red (n1 * x))
-       | C (n) -> Green (n * x))
+      | C (n) -> Green (n * x)
     in
     let id_t2 =
       match id_or_imm with
@@ -107,10 +106,7 @@ let run p e reg mem = match e with
       | C (n) -> string_of_int n
     in
     (match destld, offsetld with
-     | Green (n1), Green (n2)
-     | LightGreen (n1), LightGreen (n2)
-     | LightGreen (n1), Green (n2)
-     | Green (n1), LightGreen (n2) ->
+     | Green (n1), Green (n2) | LightGreen (n1), LightGreen (n2) | LightGreen (n1), Green (n2)| Green (n1), LightGreen (n2) ->
        begin match mem.(n1 + n2) with
          | Green n | LightGreen n as value ->
            Logs.debug (fun m ->
@@ -123,18 +119,14 @@ let run p e reg mem = match e with
                  id_t id_t2 (value_of destld) (value_of offsetld) n);
            Not_specialized (Ld (zero, C (n1 + n2 / x) , x), Red n)
        end
-     | Green (n1), Red (n2) | LightGreen (n1), Red (n2) -> failwith "Ld (green, red)"
+     | Green (n1), Red (n2) | LightGreen (n1), Red (n2) ->
+       failwith "Ld (green, red)"
      | Red (n1), Green (n2) | Red (n1), LightGreen (n2) ->
        let n = mem.(n1 + n2) in
        Logs.debug (fun m ->
            m "Ld (%s, %s), %d %d => %d: Red, Green"
              id_t id_t2 (value_of destld) (value_of offsetld) (value_of n));
-       begin match mem.(n1 + n2) with
-         | Green (n) | LightGreen (n) ->
-           Not_specialized (Ld (id_t, C (n1 + n2 / x), x), Red (n))
-         | Red (n) ->
-           Not_specialized (Ld (id_t, C (n1 + n2 / x), x), Red (n))
-       end
+       Not_specialized (Ld (id_t, C (n1 + n2 / x), x), mem.(n1 + n2))
      | Red (n1), Red (n2) ->
        let n = mem.(n1 + n2) in
        Logs.debug (fun m ->
