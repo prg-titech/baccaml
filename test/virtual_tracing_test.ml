056734dc8fcaf3866b1ac7e90f6542353e501ff1
Yusuke Izawa <yuizalp@gmail.com>
2018-08-24 13:27:11 +0900

    update virtual_tracing_test

--- test/virtual_tracing_test.ml
+++ test/virtual_tracing_test.ml
@@ -6,13 +6,14 @@ open Jit_config
 let _ =
   run begin fun arg ->
     let { prog; reg; mem; red_args; ex_name } = prepare_env arg in
-
+    let ex_name' = ("min_caml_" ^ ex_name |> Id.genid) in
     let trace =
       Jit_tracing.run_while
-        prog reg mem ("min_caml_" ^ ex_name |> Id.genid)
+        prog reg mem ex_name'
         ("stack" :: red_args) 2 0
     in
-    Logs.debug (fun m -> m "%s" (Emit_virtual.to_string_fundef trace));
+
+    print_endline (Emit_virtual.to_string_fundef trace);
 
     Jit_emit.emit_result_mj ~prog:prog ~traces:[trace] ~file:ex_name
   end
