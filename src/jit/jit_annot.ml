d90bdd0e409c20d8e7359b82f8d5cbab0b0771e9
Yusuke Izawa <yuizalp@gmail.com>
2018-09-23 20:45:58 +0900

    update jit_annot

--- src/jit/jit_annot.ml
+++ src/jit/jit_annot.ml
@@ -1,12 +1,22 @@
 open MinCaml
 open Asm
+open Operands
 
-let find_fundef name fundefs =
-  List.find begin fun fundef ->
-    let Id.L (n) = fundef.name in
-    List.for_all (fun x -> String.contains n x) (Stringext.to_list name)
-  end fundefs
+let rec trans_mj_call_ret = function
+  | Ans (IfEq (id_t, id_or_imm, t1, t2) as exp)
+  | Ans (IfLE (id_t, id_or_imm, t1, t2) as exp)
+  | Ans (IfGE (id_t, id_or_imm, t1, t2) as exp) -> Ans (exp |%%| (trans_mj_call_ret t1, trans_mj_call_ret t2))
+  | Ans (exp) -> Ans (exp)
+  | Let ((dest, typ), CallDir (Id.L ("min_caml_mj_call_start"), _, _), t) -> trans_mj_call_ret t
+  | Let ((dest, typ), CallDir (Id.L ("min_caml_mj_ret_start"), _, _), t) -> trans_mj_call_ret t
+  | Let ((dest, typ), exp, Let ((dest', typ'), CallDir (Id.L ("min_caml_mj_call_end"), _, _), t')) -> Ans (exp)
+  | Let ((dest, typ), exp, Let ((dest', typ'), CallDir (Id.L ("min_caml_mj_ret_end"), _, _), t')) -> Ans (exp)
+  | Let ((dest, typ), exp, t) -> Let ((dest, typ), exp, trans_mj_call_ret t)
 
-let transform (Prog (_, fundefs, _)) =
-  let interp = find_fundef "interp" fundefs in
-  ()
+let trans_mj (Prog (table, fundefs, main)) =
+  let { name; args; fargs; body; ret } = find_fundef "interp" fundefs in
+  let other_fundefs = List.filter (fun fundef -> fundef.name <> name ) fundefs in
+  let new_fundefs =
+    { name = name; args = args; fargs = fargs; ret = ret;
+      body = trans_mj_call_ret body; } :: other_fundefs
+  in Prog (table, new_fundefs, main)
