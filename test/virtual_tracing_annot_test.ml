f4d97229e416d05188c6359614192a58f48d9f85
Yusuke Izawa <yuizalp@gmail.com>
2018-09-23 21:03:49 +0900

    update test

--- test/virtual_tracing_annot_test.ml
+++ test/virtual_tracing_annot_test.ml
@@ -4,5 +4,11 @@ open BacCaml
 let _ =
   let ic = open_in "./test/virtual_tracing_annot.ml" in
   let lexbuf = Lexing.from_channel ic in
-  let p = Util.virtualize lexbuf |> Jit_annot.trans_mj |> Simm.f in
-  Emit_virtual.to_string_prog p |> print_endline
+  let p = Util.virtualize lexbuf |> Simm.f in
+  let p_mj = p |> Jit_annot.trans_mj in
+  let p_tj = p |> Jit_annot.trans_tj in
+  print_endline "[FOR MJ]";
+  Emit_virtual.to_string_prog p_mj |> print_endline;
+  print_newline ();
+  print_endline "[FOR TJ]";
+  Emit_virtual.to_string_prog p_tj |> print_endline
