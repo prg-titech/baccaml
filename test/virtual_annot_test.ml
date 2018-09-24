ce28d1b5785b90d9e347a9a120e0541d5b2f2a76
Yusuke Izawa <yuizalp@gmail.com>
2018-09-25 02:15:59 +0900

    fix for meta-hybrid jit

--- test/virtual_annot_test.ml
+++ test/virtual_annot_test.ml
@@ -1,6 +1,7 @@
 open MinCaml
-open BacCaml
 open RCaml
+open BacCaml
+open Jit_config
 
 let _ =
   let ic = open_in "./test/virtual_annot.ml" in
@@ -17,11 +18,13 @@ let _ =
 let _ =
   run begin fun jittype arg ->
     let { prog; reg; mem; red_args; ex_name } = prepare_env jittype arg in
-    let trace =
-      Jit_tracing.run_while
-        prog reg mem "min_caml_test_trace"
-        (red_args @ ["bytecode"]) 3 0
+    let trace = match jittype with
+      | `Meta_tracing ->
+        let res = Jit_tracing.run_while prog reg mem "min_caml_test_trace" (red_args @ ["bytecode"]) 3 0 in
+        [res]
+      | `Meta_method ->
+        Jit_method.run_while prog reg mem "min_caml_test_trace" ("stack" :: red_args)
     in
-    print_endline (Emit_virtual.to_string_fundef trace);
-    Jit_emit.emit_result ~prog:prog ~traces:[trace] ~file:ex_name ~jit_type:jittype
+    List.iter (fun t -> Emit_virtual.to_string_fundef t |> print_endline) trace;
+    Jit_emit.emit_result ~prog:prog ~traces:trace ~file:ex_name ~jit_type:jittype
   end
