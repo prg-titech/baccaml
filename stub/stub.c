fceb5b48452fe9aa7546eae53c1b498f4ba5cc06
Yusuke Izawa <yuizalp@gmail.com>
2018-10-06 02:36:40 +0900

    fix stub.c

--- stub/stub.c
+++ stub/stub.c
@@ -10,10 +10,10 @@ extern void min_caml_loop_end() asm ("min_caml_loop_end");
 
 extern void min_caml_start(char *, char *);
 
-/* "stderr" is a macro and cannot be referred to in libmincaml.S, so
-   this "min_caml_stderr" is used (in place of "__iob+32") for better
-   portability (under SPARC emulators, for example).  Thanks to Steven
-   Shaw for reporting the problem and proposing this solution. */
+/* "stderr" is a macro and cannot be referred to in libmincaml.S, so */
+/*    this "min_caml_stderr" is used (in place of "__iob+32") for better */
+/*    portability (under SPARC emulators, for example).  Thanks to Steven */
+/*    Shaw for reporting the problem and proposing this solution. */
 FILE *min_caml_stderr;
 
 // sleep in 1 second
