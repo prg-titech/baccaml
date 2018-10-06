04c5c18fad10616c6a213b4a7e2b507976d23bb8
Yusuke Izawa <yuizalp@gmail.com>
2018-10-06 21:51:51 +0900

    apply annotation transformer to base

--- src/base/min_caml.ml
+++ src/base/min_caml.ml
@@ -3,12 +3,22 @@ open MinCaml
 
 let is_dump = ref `NG
 
+let jit_typ = ref `Not_specified
+
+let id x = x
+
+let annot p = match !jit_typ with
+  | `Meta_method -> Annot.trans_mj p
+  | `Meta_tracing -> Annot.trans_tj p
+  | `Not_specified -> p
+
 let run_dump f =
   let inchan = In_channel.create (f ^ ".ml") in
   try
     Lexing.from_channel inchan
     |> Util.virtualize
     |> Trim.f
+    |> annot
     |> Simm.f
     |> Emit_virtual.to_string_prog
     |> print_endline;
@@ -24,17 +34,23 @@ let run_compile f =
     Lexing.from_channel inchan
     |> Util.virtualize
     |> Trim.f
+    |> annot
     |> Simm.f
     |> RegAlloc.f
     |> Emit.f outchan;
     In_channel.close inchan;
-    Out_channel.close outchan;
+    Out_channel.close outchan
   with e -> (In_channel.close inchan; Out_channel.close outchan; raise e)
 
 let spec_list = [
   ("-inline", Arg.Int(fun i -> Inline.threshold := i), "maximum size of functions inlined");
   ("-iter", Arg.Int(fun i -> Util.limit := i), "maximum number of optimizations iterated");
+  ("-type", Arg.String(fun str -> match str with
+       | "mjit" -> jit_typ := `Meta_method
+       | "tjit" -> jit_typ := `Meta_tracing
+       | _ -> ()), "specify jit type");
   ("-dump", Arg.Unit(fun _ -> is_dump := `OK), "emit virtual machine code");
+
 ]
 
 let usage =
