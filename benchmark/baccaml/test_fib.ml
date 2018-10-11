7ccff4f2a5ed70c175424b423daa13260e670991
Yusuke Izawa <yuizalp@gmail.com>
2018-10-12 06:22:53 +0900

    add test_fib

---
+++ benchmark/baccaml/test_fib.ml
@@ -0,0 +1,20 @@
+open MinCaml
+open RCaml
+open BacCaml
+open Jit_config
+
+let _ =
+  run begin fun jittype arg ->
+    let { prog; reg; mem; red_args; ex_name } = prepare_env jittype arg in
+    let trace = match jittype with
+      | `Meta_tracing ->
+        let res = Jit_tracing.run_while prog reg mem
+            "min_caml_test_trace" (red_args @ ["bytecode"]) 3 0 in
+        [res]
+      | `Meta_method ->
+        Jit_method.run_while prog reg mem "min_caml_test_trace"
+          ("stack" :: red_args)
+    in
+    Jit_emit.emit_result ~prog:prog ~traces:trace ~file:ex_name
+      ~jit_type:jittype
+  end
