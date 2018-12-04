396ccf065ad86aaa1ab45c867f6694afd8a7df34
Yusuke Izawa <yuizalp@gmail.com>
2018-12-04 21:35:47 +0900

    remove comments

--- test/virtual_annot_test.ml
+++ test/virtual_annot_test.ml
@@ -3,18 +3,6 @@ open RCaml
 open BacCaml
 open Jit_config
 
-(* let _ =
- *   let ic = open_in "./test/virtual_annot.ml" in
- *   let lexbuf = Lexing.from_channel ic in
- *   let p = Util.virtualize lexbuf |> Simm.f in
- *   let p_mj = p |> Jit_annot.trans_mj in
- *   let p_tj = p |> Jit_annot.trans_tj in
- *   print_endline "[FOR MJ]";
- *   Emit_virtual.to_string_prog p_mj |> print_endline;
- *   print_newline ();
- *   print_endline "[FOR TJ]";
- *   Emit_virtual.to_string_prog p_tj |> print_endline *)
-
 let _ =
   run begin fun jittype arg ->
     let { prog; reg; mem; red_args; ex_name } = prepare_env jittype arg in
