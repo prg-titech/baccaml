f1a3279484455d5bfdc14ed0c1bf52c539bce7a7
Yusuke Izawa <yuizalp@gmail.com>
2018-12-07 02:49:42 +0900

    add sq

---
+++ benchmark/c/sq.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "time.h"
+
+int sq(int n) {
+  int m = n;
+  for(int i = 1; i < n; i++)  {
+    m += n;
+  }
+  return m;
+}
+
+int main() {
+  int iter = 100;
+  int i = 0;
+  int s = get_micro_time();
+  while (i < iter) {
+    int res = sq(500000);
+    i++;
+  }
+  int e = get_micro_time();
+  printf("%f \tus\n", (e - s) / (float)iter);
+  return 0;
+}
