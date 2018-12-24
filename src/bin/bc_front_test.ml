c329106075f5489b99582f66b1e6e207368f19e0
Yusuke Izawa <yuizalp@gmail.com>
2018-12-25 04:21:20 +0900

    remove bc_front_test

--- src/bin/bc_front_test.ml
+++
@@ -1,6 +0,0 @@
-open Bc_front_lib
-
-let () =
-  let f= Sys.argv.(1) in
-  let exps = Bc_parser.program Bc_lexer.token (Lexing.from_channel (open_in f)) in
-  List.iter (fun exp -> Sub.print_exp exp; print_newline ()) exps
