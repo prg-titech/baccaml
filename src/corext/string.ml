222fe070a520f43d1ff2a813ae05526832fdd6a3
Yusuke Izawa <yuizalp@gmail.com>
2018-12-25 04:21:26 +0900

    update corext/string.ml

--- src/corext/string.ml
+++ src/corext/string.ml
@@ -6,3 +6,8 @@ let get_prefix str =
   String.split str ~on:'.'
   |> List.hd
   |> Option.value ~default:str
+
+let contains str1 str2 =
+  let re = Str.regexp_string str2 in
+  try ignore (Str.search_forward re str1 0); true
+  with _ -> false
