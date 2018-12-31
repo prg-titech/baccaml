1e98f2e2aa8680e2e6ddfe4cc66b20453373abca
Yusuke Izawa <yuizalp@gmail.com>
2018-12-31 18:58:05 +0900

    add jit_emit_base

---
+++ src/jit/jit_emit_base.mli
@@ -0,0 +1,9 @@
+open MinCaml
+
+type 'a env = {
+  out : string;
+  jit_typ : 'a;
+  prog : Asm.prog;
+}
+
+val emit : ?midflg:bool -> [< `Meta_tracing | `Meta_method] env -> Asm.fundef list -> unit
