f55f97913b3bdee1a21ed80bf5f6eaa766601f23
Yusuke Izawa <yuizalp@gmail.com>
2019-01-09 20:55:03 +0900

    [benchmark] update fib.ml

--- benchmark/mincaml/fib.ml
+++ benchmark/mincaml/fib.ml
@@ -2,12 +2,14 @@ let rec fib n =
   if n < 2 then 1
   else fib (n - 1) + fib (n - 2)
 in
-let rec loop_fib n =
+let rec loop_fib n m =
   if n = 0 then ()
-  else let _ = fib 28 in loop_fib (n - 1)
+  else let _ = fib m in loop_fib (n - 1) m
 in
+let m = read_int () in
+let n = read_int () in
 let start = get_micro_time () in
-loop_fib 10;
+loop_fib n m;
 let stop = get_micro_time () in
 print_int (stop - start);
 print_newline ()
