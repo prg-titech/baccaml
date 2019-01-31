5d7816e7d7ce38d9b0f32281dff0d3ad1d751212
Yusuke Izawa <yuizalp@gmail.com>
2019-01-31 21:07:58 +0900

    create stub.c and libmincaml.S in bin

---
+++ bin/stub.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <caml/mlvalues.h>
+#include <caml/callbacks.h>
+
+extern void min_caml_start(char *, char *);
+
+/* "stderr" is a macro and cannot be referred to in libmincaml.S, so */
+/*    this "min_caml_stderr" is used (in place of "__iob+32") for better */
+/*    portability (under SPARC emulators, for example).  Thanks to Steven */
+/*    Shaw for reporting the problem and proposing this solution. */
+FILE *min_caml_stderr;
+
+int main(int argv, char **argc) {
+  char *hp, *sp;
+  caml_startup(argv);
+  min_caml_stderr = stderr;
+  sp = alloca(1000000); hp = malloc(4000000);
+  if (hp == NULL || sp == NULL) {
+    fprintf(stderr, "malloc or alloca failed\n");
+    return 1;
+  }
+  /* fprintf(stderr, "sp = %p, hp = %p\n", sp, hp); */
+  min_caml_start(sp, hp);
+
+  return 0;
+}
