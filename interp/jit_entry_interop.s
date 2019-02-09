ff9fa9a78b038bdc55080bd8f10cbccba55fff0c
Yusuke Izawa <yuizalp@gmail.com>
2019-02-10 01:20:49 +0900

    fix asm libraries

--- interp/jit_entry_interop.s
+++ interp/jit_entry_interop.s
@@ -1,17 +1,38 @@
+#if defined(__MACH__)
+#define ALIGNSTACK0 andl $0xfffffff0, %esp
+#define ALIGNSTACK1 andl $0xfffffff0, %esp; pushl %eax; pushl %eax; pushl %eax
+#define ALIGNSTACK2 andl $0xfffffff0, %esp; pushl %eax; pushl %eax
+#define ALIGNSTACK3 andl $0xfffffff0, %esp; pushl %eax
+#else
+#define ALIGNSTACK0
+#define ALIGNSTACK1
+#define ALIGNSTACK2
+#define ALIGNSTACK3
+#endif
     .text
-    .globl min_caml_call_caml_dummy_fun
-min_caml_call_caml_dummy_fun:
-    pushl   %ebp
-    movl    %esp, %ebp
-    pushl   %eax
-    call    _call_caml_dummy_fun
-    movl    %ebp, %esp
-    popl    %ebp
+    .globl min_caml_can_enter_jit
+min_caml_can_enter_jit:
     ret
+    .globl min_caml_jit_merge_point
+min_caml_jit_merge_point:
+    ret
+    .globl min_caml_trace_entry
+min_caml_trace_entry:
+    ret
+    .globl min_caml_jit_dispatch
+min_caml_jit_dispatch:
+    ret
+    .globl min_caml_loop_start
+min_caml_loop_start:
+	ret
+    .globl min_caml_loop_end
+min_caml_loop_end:
+	ret
     .globl min_caml_call_caml_jit_entry
 min_caml_call_caml_jit_entry:
     pushl   %ebp
     movl    %esp, %ebp
+    ALIGNSTACK1
     pushl   %eax
     call    _call_caml_jit_entry
     movl    %ebp, %esp
