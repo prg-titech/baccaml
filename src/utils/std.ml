7f65108e03a8ea97eaa611388a0e4199674144a8
Yusuke Izawa <yuizalp@gmail.com>
2019-02-10 01:21:15 +0900

    fix utilities

--- src/utils/std.ml
+++ src/utils/std.ml
@@ -1,22 +1,22 @@
 module Array = struct
   include Array
 
+  let string_of_array f arr =
+    "[|" ^ (arr |> Array.to_list |> List.map f |> String.concat "; ") ^ "|]"
+
   let print_array f arr =
     print_string "[|";
     Array.iter (fun a -> f a; print_string "; ") arr;
     print_string "|]"
 end
-;;
 
 module String = struct
-  include String;;
+  include String
 
   let get_name x =
     x |> String.split_on_char '.' |> List.hd
 
   let get_extension x =
     x |> String.split_on_char '.' |> List.rev |> List.hd
-  ;;
 
 end
-;;
