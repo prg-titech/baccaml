b41e8bc10bf5dd1cc924e34700a13b876120f8b2
Yusuke Izawa <yuizalp@gmail.com>
2019-02-10 11:58:37 +0900

    update std.ml

--- src/utils/std.ml
+++ src/utils/std.ml
@@ -19,4 +19,11 @@ module String = struct
   let get_extension x =
     x |> String.split_on_char '.' |> List.rev |> List.hd
 
+  let contains s1 s2 =
+    let re = Str.regexp_string s2 in
+    try
+      ignore (Str.search_forward re s1 0);
+      true
+    with Not_found -> false
+
 end
