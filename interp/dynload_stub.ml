5922c1d1e791422f57ada4ac841e950d54f1dd2b
Yusuke Izawa <yuizalp@gmail.com>
2019-02-24 20:52:05 +0900

    add stubs for dynamic loading and executing

---
+++ interp/dynload_stub.ml
@@ -0,0 +1,3 @@
+external call_dlfun_arg1 : string -> string -> int -> int = "call_dlfun_arg1"
+
+external call_dlfun_arg2 : string -> string -> int -> int -> int = "call_dlfun_arg2"
---
+++ interp/dynloader.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <dlfcn.h>
+#include <caml/mlvalues.h>
+
+typedef int (*fun_arg2)(int, int);
+
+typedef int (*fun_arg1)(int);
+
+CAMLprim value call_dlfun_arg1(value filename, value funcname, value arg1) {
+  fun_arg1 sym = NULL;
+  void *handle = NULL;
+
+  handle = dlopen(String_val(filename), RTLD_LAZY);
+  if (handle == NULL) {fprintf(stderr, "error: dlopen\n"); return -1;}
+
+  sym = (fun_arg1)dlsym(handle, String_val(funcname));
+  if (sym == NULL) {fprintf(stderr, "error: dlsym\n"); return -1;}
+
+  return Val_int(sym(Int_val(arg1)));
+}
+
+CAMLprim value call_dlfun_arg2(value filename, value funcname, value arg1, value arg2) {
+  fun_arg2 sym = NULL;
+  void *handle = NULL;
+
+  handle = dlopen(String_val(filename), RTLD_LAZY);
+  if (handle == NULL) {fprintf(stderr, "error: dlopen\n"); return -1;}
+
+  sym = (fun_arg2)dlsym(handle, String_val(funcname));
+  if (sym == NULL) {fprintf(stderr, "error: dlsym\n"); return -1;}
+
+  return Val_int(sym(Int_val(arg1), Int_val(arg2)));
+}
