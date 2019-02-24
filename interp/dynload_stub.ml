094e2365d9e05a8479830747145bc1e559cfb44e
Yusuke Izawa <yuizalp@gmail.com>
2019-02-24 21:58:16 +0900

    fix dynload_stub

--- interp/dynload_stub.ml
+++ interp/dynload_stub.ml
@@ -1,3 +1,7 @@
 external call_dlfun_arg1 : string -> string -> int -> int = "call_dlfun_arg1"
 
 external call_dlfun_arg2 : string -> string -> int -> int -> int = "call_dlfun_arg2"
+
+let call_arg1 ~lib ~func ~arg1 = call_dlfun_arg1 lib func arg1
+
+let call_arg2 ~lib ~func ~arg1 ~arg2 = call_dlfun_arg2 lib func arg1 arg2
