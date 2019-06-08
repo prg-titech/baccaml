5449d09fd01576404cb03b83018b41e388f50184
Yusuke Izawa <yuizalp@gmail.com>
2019-06-08 21:09:39 +0900

    refactoring

--- interp/libmincaml.s
+++ interp/libmincaml.s
@@ -262,3 +262,35 @@ min_caml_hp:
 .globl min_caml_bp
 min_caml_bp:
     .long   0x0
+min_caml_is_mj:
+    ret
+    .globl min_caml_method_entry
+min_caml_method_entry:
+    ret
+    .globl min_caml_can_enter_jit
+min_caml_can_enter_jit:
+    ret
+    .globl min_caml_jit_merge_point
+min_caml_jit_merge_point:
+    ret
+    .globl min_caml_trace_entry
+min_caml_trace_entry:
+    ret
+    .globl min_caml_jit_dispatch
+min_caml_jit_dispatch:
+    ret
+    .globl min_caml_loop_start
+min_caml_loop_start:
+    ret
+    .globl min_caml_loop_end
+min_caml_loop_end:
+    ret
+    .globl min_caml_call_caml_jit_entry
+min_caml_call_caml_jit_entry:
+    pushl   %ebp
+    movl    %esp, %ebp
+    pushl   %eax
+    call    _call_caml_jit_entry
+    movl    %ebp, %esp
+    popl    %ebp
+    ret
--- interp/stub.c
+++ interp/stub.c
@@ -6,6 +6,26 @@
 
 extern void min_caml_start(char *, char *);
 
+value init_f(int n) {
+  return Val_int(n);
+}
+
+void call_caml_jit_entry(int **x) {
+  static value * jit_entry_closure = NULL;
+  value ml_args[6];
+  if (jit_entry_closure == NULL) {
+    jit_entry_closure = caml_named_value("jit_entry");
+  }
+  ml_args[0] = caml_alloc_array(init_f, x[0]);
+  ml_args[1] = caml_alloc_array(init_f, x[1]);
+  ml_args[2] = Val_int(x[2][0]);
+  ml_args[3] = Val_int(x[2][1]);
+  ml_args[4] = Val_hp(x[0]);
+  ml_args[5] = Val_hp(x[1]);
+  caml_callbackN(*jit_entry_closure, 6, ml_args);
+  return;
+}
+
 /* "stderr" is a macro and cannot be referred to in libmincaml.S, so */
 /*    this "min_caml_stderr" is used (in place of "__iob+32") for better */
 /*    portability (under SPARC emulators, for example).  Thanks to Steven */
