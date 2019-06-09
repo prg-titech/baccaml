755309f17b0e9fd02276f7c0b3916fbb1b631e0e
Yusuke Izawa <yuizalp@gmail.com>
2019-06-09 11:03:15 -0700

    add dynload_stub.mli [ci skip]

---
+++ interp/dynload_stub.mli
@@ -0,0 +1,3 @@
+val call_arg1 : lib:string -> func:string -> arg1:int -> int
+val call_arg2 : lib:string -> func:string -> arg1:int -> arg2:int -> int
+val call_arg3 : lib:string -> func:string -> arg1:int -> arg2:int -> arg3:int -> int
