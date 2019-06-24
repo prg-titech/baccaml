13c14ab76d3bb9b4faf07bbc1558c09997f05ace
Yusuke Izawa <yuizalp@gmail.com>
2019-06-24 08:47:48 -0700

    add a new module for managing fundef

---
+++ jit/fundef.ml
@@ -0,0 +1,17 @@
+open Base
+open Utils
+open Std
+
+type t = Asm.fundef
+
+let find (Asm.Prog (_, fundefs, _)) elem =
+  fundefs |> List.find (fun { Asm.name } -> name = elem)
+
+let find_opt (Asm.Prog (_, fundefs, _)) elem =
+  fundefs |> List.find_opt (fun { Asm.name } -> name = elem)
+
+let find_fuzzy (Asm.Prog (_, fundefs, _)) ~name =
+  fundefs |> List.find (fun {Asm.name= Id.L x} -> String.contains x name)
+
+let fund_fuzzy_opt (Asm.Prog (_, fundefs, _)) ~name =
+  fundefs |> List.find_opt (fun {Asm.name= Id.L x} -> String.contains x name)
