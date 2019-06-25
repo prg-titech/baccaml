e1ebb5b48d358cdd1525c5a8058ae26766fb3ed5
Yusuke Izawa <yuizalp@gmail.com>
2019-06-25 03:19:28 -0700

    add renaming counter

--- jit/renaming.ml
+++ jit/renaming.ml
@@ -4,11 +4,17 @@ open Jit_util
 
 type rename_env = Id.t -> Id.t
 
+let counter = ref 0
+
+let genid = fun id ->
+  incr counter;
+  id ^ "." ^ (string_of_int !counter)
+
 let empty_env id =
   failwith (Format.sprintf "empty env %s" id)
 
 let extend_env env id =
-  let newid = Id.genid id in
+  let newid = genid id in
   fun name ->
     if name = id then newid
     else env name
@@ -26,24 +32,17 @@ let rec rename_exp rename = function
   | Sub (id_t, id_or_imm) -> Sub (rename id_t, rename_id_or_imm rename id_or_imm)
   | Ld (id_t, id_or_imm, x) -> Ld (rename id_t, rename_id_or_imm rename id_or_imm, x)
   | St (src, dest, id_or_imm, x) -> St (rename src, rename dest, rename_id_or_imm rename id_or_imm, x)
-  | IfEq (id_t1, id_t2, t1, t2) ->
-     IfEq (rename id_t1, rename_id_or_imm rename id_t2, rename_t rename t1, rename_t rename t2)
-  | SIfEq (id_t1, id_t2, t1, t2) ->
-     SIfEq (rename id_t1, rename_id_or_imm rename id_t2, rename_t rename t1, rename_t rename t2)
-  | IfLE (id_t1, id_t2, t1, t2) ->
-     IfLE (rename id_t1, rename_id_or_imm rename id_t2, rename_t rename t1, rename_t rename t2)
-  | SIfLE (id_t1, id_t2, t1, t2) ->
-     SIfLE (rename id_t1, rename_id_or_imm rename id_t2, rename_t rename t1, rename_t rename t2)
-  | IfGE (id_t1, id_t2, t1, t2) ->
-     IfGE (rename id_t1, rename_id_or_imm rename id_t2, rename_t rename t1, rename_t rename t2)
-  | SIfGE (id_t1, id_t2, t1, t2) ->
-     SIfGE (rename id_t1, rename_id_or_imm rename id_t2, rename_t rename t1, rename_t rename t2)
+  | IfEq (id_t1, id_t2, t1, t2) -> IfEq (rename id_t1, rename_id_or_imm rename id_t2, rename_t rename t1, rename_t rename t2)
+  | SIfEq (id_t1, id_t2, t1, t2) -> SIfEq (rename id_t1, rename_id_or_imm rename id_t2, rename_t rename t1, rename_t rename t2)
+  | IfLE (id_t1, id_t2, t1, t2) -> IfLE (rename id_t1, rename_id_or_imm rename id_t2, rename_t rename t1, rename_t rename t2)
+  | SIfLE (id_t1, id_t2, t1, t2) -> SIfLE (rename id_t1, rename_id_or_imm rename id_t2, rename_t rename t1, rename_t rename t2)
+  | IfGE (id_t1, id_t2, t1, t2) -> IfGE (rename id_t1, rename_id_or_imm rename id_t2, rename_t rename t1, rename_t rename t2)
+  | SIfGE (id_t1, id_t2, t1, t2) -> SIfGE (rename id_t1, rename_id_or_imm rename id_t2, rename_t rename t1, rename_t rename t2)
   | CallDir (id_l, args, fargs) -> CallDir (id_l, List.map rename args, List.map rename fargs)
   | exp -> exp
 
 and rename_t env = function
-  | Ans (exp) ->
-    Ans (rename_exp env exp)
+  | Ans (exp) -> Ans (rename_exp env exp)
   | Let ((dest, typ), exp, body) ->
     let env' = extend_env env dest in
     Let ((env' dest, typ), rename_exp env' exp, rename_t env' body)
@@ -55,11 +54,8 @@ let rename_fundef ({name = name; args = args'; fargs = fargs; body = body; ret =
       args'
       ([], empty_env)
   in
-  let res =
-    { name = name
-    ; args = args
-    ; fargs = fargs
-    ; body = rename_t rename body
-    ; ret = ret
-    }
-  in res
+  { name = name
+  ; args = args
+  ; fargs = fargs
+  ; body = rename_t rename body
+  ; ret = ret }
--- jit/renaming.mli
+++ jit/renaming.mli
@@ -1,5 +1,6 @@
 open Base
 type rename_env = Id.t -> Id.t
+val counter : int ref
 val empty_env : string -> 'a
 val extend_env : (string -> string) -> string -> string -> string
 val rename_id_or_imm : (Id.t -> Id.t) -> Asm.id_or_imm -> Asm.id_or_imm
