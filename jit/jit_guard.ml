fbf176884ea8a2ddb75bb9b2a2f614ef1233122a
Yusuke Izawa <yuizalp@gmail.com>
2019-09-15 16:16:54 +0900

    fix crucial bug in jit_guard

--- jit/jit_guard.ml
+++ jit/jit_guard.ml
@@ -23,18 +23,28 @@ let rec restore reg cont ?wlist:(ws = []) = function
     end
   | hd :: tl -> restore reg cont tl
 
-let rec jmp_to_guard tname t = match t with
-  | Ans (CallDir (Id.L x, args, fargs)) when String.contains x "interp" ->
-    Ans (CallDir (Id.L ("guard_" ^ tname), args, fargs))
-  | Ans (e) -> Ans (e)
+let rec jmp_to_guard tname = function
+  | Ans (e) ->
+    begin match e with
+      | CallDir (Id.L (x), args, fargs) when String.contains x "interp" ->
+        Ans (CallDir (Id.L ("guard_" ^ tname), args, fargs))
+      | IfEq (x, y, t1, t2) | SIfEq (x, y, t1, t2) ->
+        Ans (IfEq (x, y, jmp_to_guard tname t1, jmp_to_guard tname t2))
+      | IfGE (x, y, t1, t2) | SIfGE (x, y, t1, t2) ->
+        Ans (IfGE (x, y, jmp_to_guard tname t1, jmp_to_guard tname t2))
+      | IfLE (x, y, t1, t2) | SIfLE (x, y, t1, t2) ->
+        Ans (IfLE (x, y, jmp_to_guard tname t1, jmp_to_guard tname t2))
+      | e -> Ans (e)
+    end
   | Let (x, e, t) -> Let (x, e, jmp_to_guard tname t)
 
-let create reg tj_env ?wlist:(ws = []) cont =
-  let free_vars = List.unique (fv cont) in
-  restore reg cont free_vars
+let rec create_tj reg tj_env ?wlist:(ws = []) cont =
 
-let create_tj reg tj_env ?wlist:(ws = []) cont =
   let { trace_name } = tj_env in
   let free_vars = List.unique (fv cont) in
   let t = restore reg cont free_vars in
   jmp_to_guard trace_name t
+
+let rec create reg tj_env ?wlist:(ws = []) cont =
+  let free_vars = List.unique (fv cont) in
+  restore reg cont free_vars
