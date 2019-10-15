b780f7393b4b5391108778012c515c28044b92cc
Yusuke Izawa <yuizalp@gmail.com>
2019-10-15 19:12:22 +0900

    update bytegen.ml

--- bytecomp/bytegen.ml
+++ bytecomp/bytegen.ml
@@ -9,19 +9,8 @@ let tap f x = f x; x
 
 let print_code arg =
   let print_code code =
-    let count = ref 1 in
     Array.iteri (fun i elem ->
-        if !count = 0 then
-          begin
-            count := 1;
-            Printf.printf "code.(%d) <- %d;\n" i elem
-          end
-        else
-          begin
-            decr count;
-            Printf.printf "code.(%d) <- %d;" i elem
-          end
-      ) code in
+       Printf.printf "code.(%d) <- %d;\n" i elem) code in
   let print_insts insts =
     Array.iter (fun inst ->
         Printf.printf "%s\n" (VM.show_inst inst))
