58703a41e62204c49770d0f9b24962323f43d521
Yusuke Izawa <yuizalp@gmail.com>
2019-10-16 17:29:45 +0900

    update typing.ml

--- base/typing.ml
+++ base/typing.ml
@@ -8,26 +8,25 @@ exception Error of t * Type.t * Type.t
 let extenv = ref M.empty
 
 (* for pretty printing (and type normalization) *)
-let rec deref_typ = function (* åž‹å¤‰æ•°ã‚’ä¸­èº«ã§ãŠãã‹ãˆã‚‹é–¢æ•° (caml2html: typing_deref) *)
+let rec deref_typ = function (* ·¿ÊÑ¿ô¤òÃæ¿È¤Ç¤ª¤­¤«¤¨¤ë´Ø¿ô (caml2html: typing_deref) *)
   | Type.Fun(t1s, t2) -> Type.Fun(List.map deref_typ t1s, deref_typ t2)
   | Type.Tuple(ts) -> Type.Tuple(List.map deref_typ ts)
   | Type.Array(t) -> Type.Array(deref_typ t)
   | Type.Var({ contents = None } as r) ->
-    (* Format.eprintf "uninstantiated type variable detected; assuming int@."; *)
-    r := Some(Type.Int);
-    Type.Int
+      Format.eprintf "uninstantiated type variable detected; assuming int@.";
+      r := Some(Type.Int);
+      Type.Int
   | Type.Var({ contents = Some(t) } as r) ->
-    let t' = deref_typ t in
-    r := Some(t');
-    t'
+      let t' = deref_typ t in
+      r := Some(t');
+      t'
   | t -> t
-let deref_id_typ (x, t) = (x, deref_typ t)
+let rec deref_id_typ (x, t) = (x, deref_typ t)
 let rec deref_term = function
   | Not(e) -> Not(deref_term e)
   | Neg(e) -> Neg(deref_term e)
   | Add(e1, e2) -> Add(deref_term e1, deref_term e2)
   | Sub(e1, e2) -> Sub(deref_term e1, deref_term e2)
-  | Mul(e1, e2) -> Mul(deref_term e1, deref_term e2)
   | Eq(e1, e2) -> Eq(deref_term e1, deref_term e2)
   | LE(e1, e2) -> LE(deref_term e1, deref_term e2)
   | FNeg(e) -> FNeg(deref_term e)
@@ -36,13 +35,12 @@ let rec deref_term = function
   | FMul(e1, e2) -> FMul(deref_term e1, deref_term e2)
   | FDiv(e1, e2) -> FDiv(deref_term e1, deref_term e2)
   | If(e1, e2, e3) -> If(deref_term e1, deref_term e2, deref_term e3)
-  | SIf(e1, e2, e3) -> SIf(deref_term e1, deref_term e2, deref_term e3)
   | Let(xt, e1, e2) -> Let(deref_id_typ xt, deref_term e1, deref_term e2)
   | LetRec({ name = xt; args = yts; body = e1 }, e2) ->
-    LetRec({ name = deref_id_typ xt;
-             args = List.map deref_id_typ yts;
-             body = deref_term e1 },
-           deref_term e2)
+      LetRec({ name = deref_id_typ xt;
+               args = List.map deref_id_typ yts;
+               body = deref_term e1 },
+             deref_term e2)
   | App(e, es) -> App(deref_term e, List.map deref_term es)
   | Tuple(es) -> Tuple(List.map deref_term es)
   | LetTuple(xts, e1, e2) -> LetTuple(List.map deref_id_typ xts, deref_term e1, deref_term e2)
@@ -60,106 +58,140 @@ let rec occur r1 = function (* occur check (caml2html: typing_occur) *)
   | Type.Var({ contents = Some(t2) }) -> occur r1 t2
   | _ -> false
 
-let rec unify t1 t2 = (* åž‹ãŒåˆã†ã‚ˆã†ã«ã€åž‹å¤‰æ•°ã¸ã®ä»£å…¥ã‚’ã™ã‚‹ (caml2html: typing_unify) *)
+let rec unify t1 t2 = (* ·¿¤¬¹ç¤¦¤è¤¦¤Ë¡¢·¿ÊÑ¿ô¤Ø¤ÎÂåÆþ¤ò¤¹¤ë (caml2html: typing_unify) *)
   match t1, t2 with
-  | Type.Unit, Type.Unit | Type.Bool, Type.Bool | Type.Int, Type.Int | Type.Float, Type.Float | Type.String, Type.String -> ()
+  | Type.Unit, Type.Unit | Type.Bool, Type.Bool | Type.Int, Type.Int | Type.Float, Type.Float -> ()
   | Type.Fun(t1s, t1'), Type.Fun(t2s, t2') ->
-    (try List.iter2 unify t1s t2s
-     with Invalid_argument(_) -> raise (Unify(t1, t2)));
-    unify t1' t2'
+      (try List.iter2 unify t1s t2s
+      with Invalid_argument(_) -> raise (Unify(t1, t2)));
+      unify t1' t2'
   | Type.Tuple(t1s), Type.Tuple(t2s) ->
-    (try List.iter2 unify t1s t2s
-     with Invalid_argument(_) -> raise (Unify(t1, t2)))
+      (try List.iter2 unify t1s t2s
+      with Invalid_argument(_) -> raise (Unify(t1, t2)))
   | Type.Array(t1), Type.Array(t2) -> unify t1 t2
   | Type.Var(r1), Type.Var(r2) when r1 == r2 -> ()
   | Type.Var({ contents = Some(t1') }), _ -> unify t1' t2
   | _, Type.Var({ contents = Some(t2') }) -> unify t1 t2'
-  | Type.Var({ contents = None } as r1), _ -> (* ä¸€æ–¹ãŒæœªå®šç¾©ã®åž‹å¤‰æ•°ã®å ´åˆ (caml2html: typing_undef) *)
-    if occur r1 t2 then raise (Unify(t1, t2));
-    r1 := Some(t2)
+  | Type.Var({ contents = None } as r1), _ -> (* °ìÊý¤¬Ì¤ÄêµÁ¤Î·¿ÊÑ¿ô¤Î¾ì¹ç (caml2html: typing_undef) *)
+      if occur r1 t2 then raise (Unify(t1, t2));
+      r1 := Some(t2)
   | _, Type.Var({ contents = None } as r2) ->
-    if occur r2 t1 then raise (Unify(t1, t2));
-    r2 := Some(t1)
+      if occur r2 t1 then raise (Unify(t1, t2));
+      r2 := Some(t1)
   | _, _ -> raise (Unify(t1, t2))
 
-let rec g env e = (* åž‹æŽ¨è«–ãƒ«ãƒ¼ãƒãƒ³ (caml2html: typing_g) *)
+let rec g env e = (* ·¿¿äÏÀ¥ë¡¼¥Á¥ó (caml2html: typing_g) *)
   try
     match e with
     | Unit -> Type.Unit
     | Bool(_) -> Type.Bool
     | Int(_) -> Type.Int
     | Float(_) -> Type.Float
-    | String(_) -> Type.String
     | Not(e) ->
-      unify Type.Bool (g env e);
-      Type.Bool
+        unify Type.Bool (g env e);
+        Type.Bool
     | Neg(e) ->
-      unify Type.Int (g env e);
-      Type.Int
-    | Add(e1, e2) | Sub(e1, e2) | Mul(e1, e2) -> (* è¶³ã—ç®—ï¼ˆã¨å¼•ãç®—ï¼‰ã®åž‹æŽ¨è«– (caml2html: typing_add) *)
-      unify Type.Int (g env e1);
-      unify Type.Int (g env e2);
-      Type.Int
+        unify Type.Int (g env e);
+        Type.Int
+    | Add(e1, e2) | Sub(e1, e2) -> (* Â­¤·»»¡Ê¤È°ú¤­»»¡Ë¤Î·¿¿äÏÀ (caml2html: typing_add) *)
+        unify Type.Int (g env e1);
+        unify Type.Int (g env e2);
+        Type.Int
     | FNeg(e) ->
-      unify Type.Float (g env e);
-      Type.Float
+        unify Type.Float (g env e);
+        Type.Float
     | FAdd(e1, e2) | FSub(e1, e2) | FMul(e1, e2) | FDiv(e1, e2) ->
-      unify Type.Float (g env e1);
-      unify Type.Float (g env e2);
-      Type.Float
+        unify Type.Float (g env e1);
+        unify Type.Float (g env e2);
+        Type.Float
     | Eq(e1, e2) | LE(e1, e2) ->
-      unify (g env e1) (g env e2);
-      Type.Bool
-    | If(e1, e2, e3) | SIf(e1, e2, e3) ->
-      unify (g env e1) Type.Bool;
-      let t2 = g env e2 in
-      let t3 = g env e3 in
-      unify t2 t3;
-      t2
-    | Let((x, t), e1, e2) -> (* letã®åž‹æŽ¨è«– (caml2html: typing_let) *)
-      unify t (g env e1);
-      g (M.add x t env) e2
-    | Var(x) when M.mem x env -> M.find x env (* å¤‰æ•°ã®åž‹æŽ¨è«– (caml2html: typing_var) *)
+        unify (g env e1) (g env e2);
+        Type.Bool
+    | If(e1, e2, e3) ->
+        unify (g env e1) Type.Bool;
+        let t2 = g env e2 in
+        let t3 = g env e3 in
+        unify t2 t3;
+        t2
+    | Let((x, t), e1, e2) -> (* let¤Î·¿¿äÏÀ (caml2html: typing_let) *)
+        unify t (g env e1);
+        g (M.add x t env) e2
+    | Var(x) when M.mem x env -> M.find x env (* ÊÑ¿ô¤Î·¿¿äÏÀ (caml2html: typing_var) *)
     | Var(x) when M.mem x !extenv -> M.find x !extenv
-    | Var(x) -> (* å¤–éƒ¨å¤‰æ•°ã®åž‹æŽ¨è«– (caml2html: typing_extvar) *)
-      (* Format.eprintf "free variable %s assumed as external@." x; *)
-      let t = Type.gentyp () in
-      extenv := M.add x t !extenv;
-      t
-    | LetRec({ name = (x, t); args = yts; body = e1 }, e2) -> (* let recã®åž‹æŽ¨è«– (caml2html: typing_letrec) *)
-      let r = Str.regexp "[a-z0-9]*\\_\\_to\\\\__[a-zA-Z0-9]*" in
-      if Str.string_match r x 0 then
-        let r = Str.regexp "\\_\\_to\\_\\_" in
-        let splitted = Str.split r x in
-        assert (List.length splitted = 2);
-        let from, to_ = List.(nth splitted 0, nth splitted 1) in
-        unify t (Type.(Fun ([type_of_string from], type_of_string to_)));
-        g env e2
-      else
+    | Var(x) -> (* ³°ÉôÊÑ¿ô¤Î·¿¿äÏÀ (caml2html: typing_extvar) *)
+        Format.eprintf "free variable %s assumed as external@." x;
+        let t = Type.gentyp () in
+        extenv := M.add x t !extenv;
+        t
+    | LetRec({ name = (x, t); args = yts; body = e1 }, e2) -> (* let rec¤Î·¿¿äÏÀ (caml2html: typing_letrec) *)
+
+       (* HM's hack to declare casting function.  When the function
+          name starts with "cast_" it does not type check the body but
+          assigns the type specified by the function name.
+
+        For example, "let rec cast_fIF x = x" will be typed int ->
+          float.  As it is an identity function, it will serve as a
+          casting function (for architectures that use the same
+          registers for integers and floatings). *)
+
+       (* if the name of the function (x) begins with "cast_" *)
+       if (String.length x) > 5 && (String.sub x 0 5) = "cast_" then
+
+         (* auxiliary function that converts a string to a list of chars *)
+         (let to_seq str =
+            let rec loop idx seq =
+              if idx < 0 then seq
+              else loop (idx-1) ((String.get str idx)::seq) in
+            loop (String.length str - 1) [] in
+
+          (* take out the type specification part of the function name *)
+          let typespec = (to_seq (String.sub x 5 (String.length x - 5))) in
+
+          Format.eprintf "letrec with cast name %s \n" x;
+
+          (* convert a type spec to a type value *)
+          let rec parse_type spec =
+            match spec with
+            | 'I'::r -> (Type.Int,r)
+            | 'F'::r -> (Type.Float,r)
+            | 'B'::r -> (Type.Bool,r)
+            | 'U'::r -> (Type.Unit,r)
+            | 'A'::r -> let (element_type,r) = parse_type r in
+              (Type.Array(element_type),r)
+            | 'f'::r -> let (domain,r) = parse_type r in
+              let (range,r)  = parse_type r in
+              (Type.Fun([domain],range),r)
+          in
+          let casted_type,r = parse_type typespec in
+          let env = M.add x t env in
+          unify t casted_type;  (* let t have type as specified,
+                                   without seeing the body *)
+          g env e2)
+       else
         let env = M.add x t env in
         unify t (Type.Fun(List.map snd yts, g (M.add_list yts env) e1));
         g env e2
-    | App(e, es) -> (* é–¢æ•°é©ç”¨ã®åž‹æŽ¨è«– (caml2html: typing_app) *)
-      let t = Type.gentyp () in
-      unify (g env e) (Type.Fun(List.map (g env) es, t));
-      t
+    | App(e, es) -> (* ´Ø¿ôÅ¬ÍÑ¤Î·¿¿äÏÀ (caml2html: typing_app) *)
+        let t = Type.gentyp () in
+        unify (g env e) (Type.Fun(List.map (g env) es, t));
+        t
     | Tuple(es) -> Type.Tuple(List.map (g env) es)
     | LetTuple(xts, e1, e2) ->
-      unify (Type.Tuple(List.map snd xts)) (g env e1);
-      g (M.add_list xts env) e2
+        unify (Type.Tuple(List.map snd xts)) (g env e1);
+        g (M.add_list xts env) e2
     | Array(e1, e2) -> (* must be a primitive for "polymorphic" typing *)
-      unify (g env e1) Type.Int;
-      Type.Array(g env e2)
+        unify (g env e1) Type.Int;
+        Type.Array(g env e2)
     | Get(e1, e2) ->
-      let t = Type.gentyp () in
-      unify (Type.Array(t)) (g env e1);
-      unify Type.Int (g env e2);
-      t
+        let t = Type.gentyp () in
+        unify (Type.Array(t)) (g env e1);
+        unify Type.Int (g env e2);
+        t
     | Put(e1, e2, e3) ->
-      let t = g env e3 in
-      unify (Type.Array(t)) (g env e1);
-      unify Type.Int (g env e2);
-      Type.Unit
+        let t = g env e3 in
+        unify (Type.Array(t)) (g env e1);
+        unify Type.Int (g env e2);
+        Type.Unit
   with Unify(t1, t2) -> raise (Error(deref_term e, deref_typ t1, deref_typ t2))
 
 let f e =
@@ -170,6 +202,6 @@ let f e =
   | _ -> Format.eprintf "warning: final result does not have type unit@.");
 *)
   (try unify Type.Unit (g M.empty e)
-   with Unify _ -> failwith "top level does not have type unit");
+  with Unify _ -> failwith "top level does not have type unit");
   extenv := M.map deref_typ !extenv;
   deref_term e
