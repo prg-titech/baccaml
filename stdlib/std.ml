377a8f360174ee5ce82c7c9b9df6cd937aa61ec6
Yusuke Izawa <yuizalp@gmail.com>
2019-10-21 16:30:39 +0900

    [stdlib] update stdlib

--- stdlib/std.ml
+++ stdlib/std.ml
@@ -13,9 +13,16 @@ module Array = struct
     print_string "|]"
 end
 
-module String = struct
-  include String
-
+module ExString : sig
+  (* get first element xxx of a string such as xxx.yy.zz *)
+  val get_name : string -> string
+  (* get last element zz of a string such as xxx.yy.zz *)
+  val get_extension : string -> string
+  (* check whether s1 has substring s2 *)
+  val contains : string -> string -> bool
+  (* check wheter s1 starts with substring s2 *)
+  val starts_with : string -> string -> bool
+end = struct
   let get_name x =
     x |> String.split_on_char '.' |> List.hd
 
@@ -28,11 +35,35 @@ module String = struct
       ignore (Str.search_forward re s1 0);
       true
     with Not_found -> false
+
+  let starts_with s1 s2 =
+    let re = Str.regexp_string s2 in
+    try
+      if Str.search_forward re s1 0 = 0
+      then true
+      else false
+    with Not_found -> false
+
+  let _ =
+    assert (starts_with "min_caml_print_int" "min_caml");
+    assert (not (starts_with "__min_caml_print_int" "min_caml"))
 end
 
-module List = struct
-  include List
+module String = struct
+  include String
+  include ExString
+end
 
+module ExList : sig
+  (* remove duplicated elements in a given list *)
+  val unique : 'a list -> 'a list
+  (* get a last element of list l *)
+  val last : 'a list -> 'a
+  (* get the index of an element elem in list lst *)
+  val index : 'a -> 'a list -> int
+  (* get the index of an element elem in list lst *)
+  val index_opt : 'a -> 'a list -> int option
+end = struct
   let rec unique list =
     let rec go l s =
       match l with
@@ -65,4 +96,10 @@ module List = struct
          if hd = elem then Some (i)
          else go elem tl (i + 1)
     in go elem lst 0
+end
+
+
+module List = struct
+  include List
+  include ExList
 end
