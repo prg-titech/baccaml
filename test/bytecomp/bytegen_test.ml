e6a9d9bbd3cb97518f78403aff440182462e4c5b
Yusuke Izawa <yuizalp@gmail.com>
2019-11-22 06:07:20 +0900

    uptdate bytegen_test.ml and for.bc.ml
    - add test_for_loop2 in bytegen_test.ml

--- test/bytecomp/bytegen_test.ml
+++ test/bytecomp/bytegen_test.ml
@@ -120,6 +120,25 @@ in let () = f 10
 " in
   assert_equal ~printer:string_of_int 15 (test code)
 
+let test_for_loop2 _ =
+  let code = "
+let rec fib n =
+  if n < 2 then 1
+  else fib (n-1) + fib(n-2)
+in
+let rec f n =
+  let arr = Array.make 1 0 in
+  for i = 0 to 10 do
+    let x = fib i in
+    let y = arr.(0) + x in
+    arr.(0) <- y;
+    ()
+  done;
+  arr.(0)
+in let () = f 10
+" in
+  assert_equal ~printer:string_of_int 63 (test code)
+
 let suite =
   "ByteCompilerTest" >::: [
     "test_fun1" >:: test_fun1;
@@ -137,6 +156,7 @@ let suite =
 let loop_site =
   "LazyTest" >::: [
     "test_for" >:: test_for_loop;
+    "test_for2" >:: test_for_loop2
   ]
 
 let () =
--- test/bytecomp/for.bc.ml
+++ test/bytecomp/for.bc.ml
@@ -1,7 +1,13 @@
+let rec fib n =
+  if n < 2 then 1
+  else fib (n-1) + fib(n-2)
+in
 let rec f n =
   let arr = Array.make 1 0 in
   for i = 0 to 10 do
-    arr.(0) <- i;
+    let x = fib i in
+    let y = arr.(0) + x in
+    arr.(0) <- y;
     ()
   done;
   arr.(0)
