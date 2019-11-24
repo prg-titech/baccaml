181b662cfd7417d23733eed810b6cde56ff5bf0b
Yusuke Izawa <yuizalp@gmail.com>
2019-11-24 19:49:05 +0900

    fix print_code in util.ml

--- bytecomp/util.ml
+++ bytecomp/util.ml
@@ -6,40 +6,46 @@ let rec print_insts insts =
   match insts with
   | [] -> ()
   | hd :: tl ->
-     match hd with
-     | Literal i ->
-        print_int i; print_newline ();
-        print_insts tl
-     | Lref s | Ldef s ->
-        failwith "Lref or Ldef is still remained"
-     | _ ->
-        (if List.assoc hd has_args then
-           (print_int (int_of_inst hd); print_string " ")
-         else
-           print_int (int_of_inst hd); print_newline ());
-        print_insts tl
+    match hd with
+    | Literal i ->
+      print_int i; print_newline ();
+      print_insts tl
+    | Lref s | Ldef s ->
+      failwith "Lref or Ldef is still remained"
+    | _ ->
+      (if List.assoc hd has_args then
+         (print_int (int_of_inst hd); print_string " ")
+       else
+         print_int (int_of_inst hd); print_newline ());
+      print_insts tl
 
 
 let print_code insts =
   let rec loop i = function
     | [] -> ()
     | Literal j :: tl ->
-       (printf "code.(%d) <- %d;\n" i j;
-        loop (i+1) tl)
+      (printf "code.(%d) <- %d;\n" i j;
+       loop (i+1) tl)
     | Lref _ :: tl | Ldef _ :: tl ->
-       failwith "Lref or Ldef is still remained"
+      failwith "Lref or Ldef is still remained"
     | CALL :: tl ->
-       printf "code.(%d) <- %d; " i     (int_of_inst CALL);
-       printf "code.(%d) <- %d; " (i+1) (int_of_inst (List.hd tl));
-       let tl = List.tl tl in
-       printf "code.(%d) <- %d;\n" (i+2) (int_of_inst (List.hd tl));
-       loop (i+3) (List.tl tl)
+      printf "code.(%d) <- %d; "   i     (int_of_inst CALL);
+      printf "code.(%d) <- %d; "  (i+1)  (int_of_inst (List.hd tl));
+      let tl = List.tl tl in
+      printf "code.(%d) <- %d;\n" (i+2)  (int_of_inst (List.hd tl));
+      loop (i+3) (List.tl tl)
+    | CALL_HS :: tl ->
+      printf "code.(%d) <- %d; "   i     (int_of_inst CALL_HS);
+      printf "code.(%d) <- %d; "  (i+1)  (int_of_inst (List.hd tl));
+      let tl = List.tl tl in
+      printf "code.(%d) <- %d;\n" (i+2)  (int_of_inst (List.hd tl));
+      loop (i+3) (List.tl tl)
     | hd :: tl ->
-       if List.assoc hd has_args then
-         (printf "code.(%d) <- %d; " i (int_of_inst hd))
-       else
-         (printf "code.(%d) <- %d;\n" i (int_of_inst hd));
-       loop (i+1) tl
+      if List.assoc hd has_args then
+        (printf "code.(%d) <- %d; " i (int_of_inst hd))
+      else
+        (printf "code.(%d) <- %d;\n" i (int_of_inst hd));
+      loop (i+1) tl
   in
   printf "let code = Array.make %d %d in\n" (List.length insts) 0;
   loop 0 insts; flush stdout
