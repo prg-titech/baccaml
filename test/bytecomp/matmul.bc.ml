8dd895449fab9a1075c027586c72e27f33c5da04
Yusuke Izawa <yuizalp@gmail.com>
2019-12-30 03:27:16 +0900

    Nits

--- test/bytecomp/matmul.bc.ml
+++ test/bytecomp/matmul.bc.ml
@@ -16,14 +16,12 @@ let rec loop1 i m n a b c =
    loop1 (i - 1) m n a b c) in
 let rec mul l m n a b c =
   loop1 (l - 1) m n a b c in
-
 let rec init i n mat =
   if i < 0 then () else (
     let x = Array.make n 0 in
     mat.(i) <- x;
     init (i - 1) n mat)
 in
-
 let rec make m n =
   let dummy = Array.make n 1 in
   let mat = Array.make m dummy in
@@ -38,4 +36,5 @@ let () =
   b.(0).(0) <- 7; b.(0).(1) <- 8;
   b.(1).(0) <- 9; b.(1).(1) <- 10;
   b.(2).(0) <- 11; b.(2).(1) <- 12;
-  mul 2 3 2 a b c
+  let r = mul 2 3 2 a b c in
+  r.(0).(0)
