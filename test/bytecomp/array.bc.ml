fd29c86bddc914bf0e41c4de240346d962507eb7
Yusuke Izawa <yuizalp@gmail.com>
2019-12-30 19:15:37 +0900

    Fix a test case for bytecomp

--- test/bytecomp/array.bc.ml
+++ test/bytecomp/array.bc.ml
@@ -1,5 +1,6 @@
 let rec simple x y =
-  let a = Array.make 10 0 in
-  a.(5) <- x; a.(7) <- y;
-  a.(5) + a.(7)
-in let () = simple 10 20
+  let arr = Array.make 10 0 in
+  arr.(5) <- x; arr.(7) <- y;
+  arr.(5) + arr.(7)
+in
+let () = simple 10 20
