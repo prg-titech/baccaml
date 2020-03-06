8efdef4152cc6d0d4418a3e9687f31be87c2ce93
Yusuke Izawa <yuizalp@gmail.com>
2020-03-06 16:30:12 +0900

    Fix jit_constfold

--- jit/jit_constfold.ml
+++ jit/jit_constfold.ml
@@ -2,74 +2,135 @@ open Std
 open MinCaml
 open Asm
 
-let ( /=/ ) s1 s2 = String.(get_name s1 = get_name s2)
+let ( |=| ) s1 s2 = String.(get_name s1 = get_name s2)
 
-let rec replace trg = function
-  | Ans e -> Ans (replace_exp trg e)
-  | Let ((x, typ), e, t) -> Let ((x, typ), replace_exp trg e, replace trg t)
-
-and replace_exp trg = function
-  | Mov x when trg /=/ x -> Mov trg
-  | Add (x, y) when x /=/ trg -> Add (trg, y)
-  | Sub (x, y) when x /=/ trg -> Add (trg, y)
-  | Mul (x, y) when x /=/ trg -> Add (trg, y)
-  | IfEq (x, y, t1, t2) when x /=/ trg ->
-    IfEq (trg, y, replace trg t1, replace trg t2)
-  | IfLE (x, y, t1, t2) when x /=/ trg ->
-    IfLE (trg, y, replace trg t1, replace trg t2)
-  | IfGE (x, y, t1, t2) when x /=/ trg ->
-    IfGE (trg, y, replace trg t1, replace trg t2)
-  | IfEq (x, y, t1, t2) -> IfEq (x, y, replace trg t1, replace trg t2)
-  | IfLE (x, y, t1, t2) -> IfLE (x, y, replace trg t1, replace trg t2)
-  | IfGE (x, y, t1, t2) -> IfGE (x, y, replace trg t1, replace trg t2)
-  | SIfEq (x, y, t1, t2) when x /=/ trg ->
-    SIfEq (trg, y, replace trg t1, replace trg t2)
-  | SIfLE (x, y, t1, t2) when x /=/ trg ->
-    SIfLE (trg, y, replace trg t1, replace trg t2)
-  | SIfGE (x, y, t1, t2) when x /=/ trg ->
-    SIfGE (trg, y, replace trg t1, replace trg t2)
-  | SIfEq (x, y, t1, t2) -> SIfEq (x, y, replace trg t1, replace trg t2)
-  | SIfLE (x, y, t1, t2) -> SIfLE (x, y, replace trg t1, replace trg t2)
-  | SIfGE (x, y, t1, t2) -> SIfGE (x, y, replace trg t1, replace trg t2)
-  | Ld (x, y, n) when x /=/ trg -> Ld (trg, y, n)
-  | St (x, y, z, n) when x /=/ trg -> St (trg, y, z, n)
-  | St (x, y, z, n) when y /=/ trg -> St (x, trg, z, n)
-  | CallCls (x, ys, zs) ->
-    CallCls
-      ( x
-      , List.map (fun y -> if y /=/ trg then trg else y) ys
-      , List.map (fun z -> if z /=/ trg then trg else z) zs )
-  | CallDir (x, ys, zs) ->
-    CallDir
-      ( x
-      , List.map (fun y -> if y /=/ trg then trg else y) ys
-      , List.map (fun z -> if z /=/ trg then trg else z) zs )
-  | exp -> exp
-;;
-
-let can_folding trg = function
-  | Mov x when trg /=/ x -> `Folding x
-  | exp -> `Not_folding
-;;
-
-let rec folding = function
-  | Ans (IfEq (x, y, t1, t2)) -> Ans (IfEq (x, y, folding t1, folding t2))
-  | Ans (IfLE (x, y, t1, t2)) -> Ans (IfLE (x, y, folding t1, folding t2))
-  | Ans (IfGE (x, y, t1, t2)) -> Ans (IfGE (x, y, folding t1, folding t2))
-  | Ans e -> Ans e
+let rec replace var t =
+  match t with
+  | Ans e -> (match replace_exp var e with Some exp -> Ans exp | None -> Ans Nop)
   | Let ((x, typ), e, t) ->
-    (match can_folding x e with
-    | `Folding x -> replace x t
-    | `Not_folding -> Let ((x, typ), e, folding t))
+    (match replace_exp var e with
+    | Some e -> Let ((x, typ), e, replace var t)
+    | None -> replace var t)
+
+and replace_exp var exp =
+  match exp with
+  | Nop | Set _ | SetL _ -> Some exp
+  | Mov id | SMov id | Neg id -> if id |=| var then None else Some exp
+  | Add (x, V y) ->
+    Some
+      (match x |=| var, y |=| var with
+      | true, true -> Add (var, V var)
+      | true, false -> Add (var, V y)
+      | false, true -> Add (x, V var)
+      | false, false -> Add (x, V y))
+  | Add (x, C y) -> Some (if x |=| var then Add (var, C y) else Add (x, C y))
+  | Sub (x, V y) ->
+    Some
+      (match x |=| var, y |=| var with
+      | true, true -> Sub (var, V var)
+      | true, false -> Sub (var, V y)
+      | false, true -> Sub (x, V var)
+      | false, false -> Sub (x, V y))
+  | Sub (x, C y) -> Some (if x |=| var then Sub (var, C y) else Sub (x, C y))
+  | Mul (x, V y) ->
+    Some
+      (match x |=| var, y |=| var with
+      | true, true -> Mul (var, V var)
+      | true, false -> Mul (var, V y)
+      | false, true -> Mul (x, V var)
+      | false, false -> Mul (x, V y))
+  | Mul (x, C y) -> Some (if x |=| var then Mul (var, C y) else Mul (x, C y))
+  | Div (x, V y) ->
+    Some
+      (match x |=| var, y |=| var with
+      | true, true -> Mul (var, V var)
+      | true, false -> Div (var, V y)
+      | false, true -> Div (x, V var)
+      | false, false -> Div (x, V y))
+  | Div (x, C y) -> Some (if x |=| var then Div (var, C y) else Div (x, C y))
+  | Mod (x, V y) ->
+    Some
+      (match x |=| var, y |=| var with
+      | true, true -> Mul (var, V var)
+      | true, false -> Mod (var, V y)
+      | false, true -> Mod (x, V var)
+      | false, false -> Mod (x, V y))
+  | Mod (x, C y) -> Some (if x |=| var then Mod (var, C y) else Mod (x, C y))
+  | Ld (x, V y, z) ->
+    Some
+      (match x |=| var, y |=| var with
+      | true, true -> Ld (var, V var, z)
+      | true, false -> Ld (var, V y, z)
+      | false, true -> Ld (x, V var, z)
+      | false, false -> Ld (x, V y, z))
+  | Ld (x, C y, z) -> Some (if x |=| var then Ld (var, C y, z) else Ld (x, C y, z))
+  | St (x, y, V z, w) ->
+    Some
+      (match y |=| var, z |=| var with
+      | true, true -> St (x, var, V var, w)
+      | true, false -> St (x, var, V z, w)
+      | false, true -> St (x, y, V var, w)
+      | false, false -> St (x, y, V z, w))
+  | St (x, y, C z, w) ->
+    Some (if y |=| var then St (x, var, C z, w) else St (x, y, C z, z))
+  | IfEq (x, V y, t1, t2) ->
+    Some
+      (match x |=| var, y |=| var with
+      | true, true -> IfEq (var, V var, replace var t1, replace var t2)
+      | true, false -> IfEq (var, V y, replace var t1, replace var t2)
+      | false, true -> IfEq (x, V var, replace var t1, replace var t2)
+      | false, false -> IfEq (x, V y, replace var t1, replace var t2))
+  | IfEq (x, C y, t1, t2) ->
+    Some
+      (if x |=| var
+      then IfEq (var, C y, replace var t1, replace var t2)
+      else IfEq (x, C y, replace var t1, replace var t2))
+  | IfLE (x, V y, t1, t2) ->
+    Some
+      (match x |=| var, y |=| var with
+      | true, true -> IfLE (var, V var, replace var t1, replace var t2)
+      | true, false -> IfLE (var, V y, replace var t1, replace var t2)
+      | false, true -> IfLE (x, V var, replace var t1, replace var t2)
+      | false, false -> IfLE (x, V y, replace var t1, replace var t2))
+  | IfLE (x, C y, t1, t2) ->
+    Some
+      (if x |=| var
+      then IfLE (var, C y, replace var t1, replace var t2)
+      else IfLE (x, C y, replace var t1, replace var t2))
+  | IfGE (x, V y, t1, t2) ->
+    Some
+      (match x |=| var, y |=| var with
+      | true, true -> IfGE (var, V var, replace var t1, replace var t2)
+      | true, false -> IfGE (var, V y, replace var t1, replace var t2)
+      | false, true -> IfGE (x, V var, replace var t1, replace var t2)
+      | false, false -> IfGE (x, V y, replace var t1, replace var t2))
+  | IfGE (x, C y, t1, t2) ->
+    Some
+      (if x |=| var
+      then IfGE (var, C y, replace var t1, replace var t2)
+      else IfGE (x, C y, replace var t1, replace var t2))
+  | _ -> failwith "un matched pattern."
 ;;
 
-let rec iter ?(n = 2) t =
-  let r = folding t in
-  if t = r then r else if n = 0 then r else iter ~n:(n - 1) r
-;;
-
-let rec iter_fundef ?(n = 2) fundef =
-  let { name; args; fargs; body; ret } = fundef in
-  let body = iter ~n body in
-  { name; args; fargs; body; ret }
+let%test _ =
+  let open Type in
+  let t = Let (("y.1", Int), Add ("x.0", C (1)),
+               Let (("stack.379.209", Unit), Mov ("stack.379"),
+                    Let (("v.11", Int), Ld ("stack.379.209", V "y.1", 4),
+                         Ans (IfEq ("x.0", C (1),
+                                    Ans (Mov "y.1"),
+                                    Let (("z.10", Int), Add ("x.0", V ("y.1")),
+                                         Ans (Mov ("z.10")))))))) in
+  let res = replace "stack.379" t in
+  let expected =
+    (Asm.Let (("y.1", Type.Int), (Asm.Add ("x.0", (Asm.C 1))),
+              (Asm.Let (("v.11", Type.Int), (Asm.Ld ("stack.379", (Asm.V "y.1"), 4)),
+                        (Asm.Ans
+                           (Asm.IfEq ("x.0", (Asm.C 1), (Asm.Ans (Asm.Mov "y.1")),
+                                      (Asm.Let (("z.10", Type.Int), (Asm.Add ("x.0", (Asm.V "y.1"))),
+                                                (Asm.Ans (Asm.Mov "z.10"))))
+                                     )))
+                       ))
+             )) in
+  expected = res
 ;;
--- jit/jit_constfold.mli
+++ jit/jit_constfold.mli
@@ -1,6 +1,2 @@
 open MinCaml
 open Asm
-
-val folding : t -> t
-val iter : ?n:int -> t -> t
-val iter_fundef : ?n:int -> fundef -> fundef
