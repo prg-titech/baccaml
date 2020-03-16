bfc20ef82f991831e44f647325e7e0cb192c65cd
Yusuke Izawa <yuizalp@gmail.com>
2020-03-16 16:52:28 +0900

    Add an optimization for removing useless Mov

--- opt/opt_defuse.ml
+++ opt/opt_defuse.ml
@@ -18,6 +18,7 @@ let rec specialize_arith lhs rhs =
   | Sub (x, C n), Sub (y, C m) -> Some (Sub (x, C (n + m)))
   | Add (x, C n), Sub (y, C m) -> Some (Add (x, C (n - m)))
   | Sub (x, C n), Add (y, C m) -> Some (Sub (x, C (n - m)))
+  | e, Mov x -> Some e
   | _ -> None
 ;;
 
@@ -65,19 +66,25 @@ let constfold_arith t =
   let rec constfold_arith' env acc = function
     | Let ((var, typ), e, t) ->
       (match e with
-      | Add (x, y) | Sub (x, y) ->
-        (match M.find_opt x env with
-        | Some e_lhs ->
-          Printf.eprintf "e_trg: %s\n" (show_exp e_lhs);
-          let acc = remove_and_specialize e_lhs e acc in
-          Printf.eprintf "acc: %s\n" (show acc);
-          constfold_arith' env acc t
-        | None ->
-          let env = M.add var e env in
-          constfold_arith' env acc t)
-      | _ ->
-        let env = M.add var e env in
-        constfold_arith' env acc t)
+       | Mov x ->
+         (match M.find_opt x env with
+          | Some e_lhs ->
+            let acc = remove_and_specialize e_lhs e acc in
+            constfold_arith' env acc t
+          | None ->
+            let env = M.add var e env in
+            constfold_arith' env acc t)
+       | Add (x, y) | Sub (x, y) ->
+         (match M.find_opt x env with
+          | Some e_lhs ->
+            let acc = remove_and_specialize e_lhs e acc in
+            constfold_arith' env acc t
+          | None ->
+            let env = M.add var e env in
+            constfold_arith' env acc t)
+       | _ ->
+         let env = M.add var e env in
+         constfold_arith' env acc t)
     | Ans e -> acc
   in
   constfold_arith' M.empty t t
@@ -97,10 +104,37 @@ let%test "constfold_arith test1" =
               , Let
                   ( ("Tu24.613", Unit)
                   , St ("v.612", "stack.399", V "sp.400", 4)
-                  , Let (("Ti246.615", Int), Add ("sp.400", C 1), Ans (Set (-100))) ) ) )
+                  , Let (("Ti246.615", Int), Add ("sp.400", C 1),
+                         Ans (Set (-100))) ) ) )
       )
   in
-  constfold_arith t_wo_if |> show |> print_endline;
+  let t_trace1 =
+     Let (("Ti242.609", Int),  Sub ("sp.400",C 2 ),
+     Let (("Ti244.611", Int),  Sub ("Ti242.609",C 1 ),
+     Let (("v.612", Int),  Ld ("stack.399", V ("Ti244.611"),4),
+     Let (("Tu24.613", Unit),  St ("v.612","stack.399",V ("sp.400"),4),
+     Let (("Ti246.615", Int),  Add ("sp.400",C 1 ),
+     Let (("sp.400.848", Int),  Mov ("Ti246.615"),
+     Let (("Ti333.507.868", Int),  Set (0),
+     Let (("Tu32.508.869", Unit),  St ("Ti333.507.868","stack.399",V ("sp.400.848"),4),
+     Let (("Ti335.510.870", Int),  Add ("sp.400.848",C 1 ),
+     Let (("sp.400.1071", Int),  Mov ("Ti335.510.870"),
+     Let (("Ti149.724.1225", Int),  Sub ("sp.400.1071",C 1 ),
+     Let (("v2.725.1226", Int),  Ld ("stack.399",V ("Ti149.724.1225"),4),
+     Let (("Ti151.727.1227", Int),  Sub ("sp.400.1071",C 2 ),
+     Let (("v1.728.1228", Int),  Ld ("stack.399",V ("Ti151.727.1227"), 4),
+     Let (("n.729.1229", Int),  IfLE ("v1.728.1228",V ("v2.725.1226"),
+                                        Ans (Set (1)),
+                                        Ans (Set (0))),
+     Let (("Ti153.731.1230", Int),  Sub ("sp.400.1071",C 2 ),
+     Let (("Tu12.732.1231", Unit),  St ("n.729.1229","stack.399",V ("Ti153.731.1230"),4),
+     Let (("Ti155.734.1232", Int),  Sub ("sp.400.1071",C 1 ),
+     Let (("sp.400.1294", Int),  Mov ("Ti155.734.1232"),
+     Let (("Ti191.680.1421", Int),  Sub ("sp.400.1294",C 1 ),
+     Let (("v.681.1422", Int),  Ld ("stack.399",V ("Ti191.680.1421"),4),
+     Let (("sp2.683.1423", Int),  Sub ("sp.400.1294",C 1 ),
+     Ans (Set 100))))))))))))))))))))))) in
+  constfold_arith t_trace1 |> show |> print_endline;
   true
 ;;
 
