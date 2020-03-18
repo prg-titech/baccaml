7a264a6e25e066f5da1ea4baf3b0367b57b0a73d
Yusuke Izawa <yuizalp@gmail.com>
2020-03-18 17:41:24 +0900

    Update const_fold_mov

--- opt/opt_defuse.ml
+++ opt/opt_defuse.ml
@@ -157,10 +157,6 @@ module Opt = struct
   ;;
 
   let rec const_fold env = function
-    | Let ((var, typ), Mov (x), t) when exists_var x env ->
-      let exp' = M.find x env in
-      let env = extend_env var exp' env in
-      Let ((var, typ), exp', const_fold env t)
     | Let ((var, typ), Add (x, y), t) when exists_var x env ->
       let lhs = M.find x env in
       (match specialize lhs (Add (x, y)) with
@@ -171,9 +167,7 @@ module Opt = struct
         let env = extend_env var (Add (x, y)) env in
         Let ((var, typ), Add (x, y), const_fold env t))
     | Let ((var, typ), Sub (x, y), t) when exists_var x env ->
-      ep "Sub x: %s\n" x;
       let lhs = M.find x env in
-      ep "exp': %s\n" (show_exp lhs);
       (match specialize lhs (Sub (x, y)) with
       | Some e_res ->
         let env = extend_env var e_res env in
@@ -202,6 +196,101 @@ module Opt = struct
       end
   ;;
 
+  let rec replace_var var1 e_mov e =
+    match e_mov with
+    | Mov var2 -> (
+        ep "var1: %s, var2: %s\n" var1 var2;
+        match e with
+        | Nop -> Nop
+        | Mov x -> if x = var1 then Mov var2 else Mov x
+        | Add (x, V y) ->
+          if x = var1 then Add (var2, V y)
+          else if y = var1 then Add (x, V var2)
+          else Add (x, V y)
+        | Add (x, C n) ->
+          if x = var1 then Add (var2, C n) else Add (x, C n)
+        | Sub (x, V y) ->
+          if x = var1 then Sub (var2, V y)
+          else if y = var1 then Sub (x, V var2)
+          else Sub (x, V y)
+        | Sub (x, C n) ->
+          if x = var1 then Sub (var2, C n) else Sub (x, C n)
+        | Ld (x, V y, z) ->
+          if x = var1 then Ld (var2, V y, z)
+          else if y = var1 then Ld (x, V var2, z)
+          else Ld (x, V y, z)
+        | Ld (x, C y, z) ->
+          if x = var1 then Ld (var2, C y, z) else Ld (x, C y, z)
+        | St (x, y, V z, w) ->
+          if x = var1 then St (var2, y, V z, w)
+          else if y = var1 then St (x, var2, V z, w)
+          else if z = var1 then St (x, y, V var2, w)
+          else St (x, y, V z, w)
+        | St (x, y, C n, w) ->
+          if x = var1 then St (var2, y, C n, w)
+          else if y = var1 then St (x, var2, C n, w)
+          else St (x, y, C n, w)
+        | CallDir (id_l, args, fargs) ->
+          CallDir (
+            id_l,
+            args |> List.map (fun x -> if x = var1 then var2 else x),
+            fargs |> List.map (fun x -> if x = var1 then var2 else x))
+        | CallCls (x, args, fargs) ->
+          CallCls (
+            (if x = var1 then var1 else x),
+            args |> List.map (fun x -> if x = var1 then var2 else x),
+            fargs |> List.map (fun x -> if x = var1 then var2 else x))
+        | IfEq (x, V y, t1, t2) | IfLE (x, V y, t1, t2) | IfGE (x, V y, t1, t2) ->
+          if x = var1 then e <=> (var2, V y, t1, t2)
+          else if y = var1 then e <=> (x, V var2, t1, t2)
+          else e <=> (x, V y, t1, t2)
+        | _ -> e)
+    | _ -> e
+  ;;
+
+  let rec const_fold_mov env = function
+    | Let ((var, typ), Mov x, t) ->
+      ep "var: %s, x: %s\n" var x;
+      let env = extend_env var x env in
+      const_fold_mov env t
+    | Let ((var, typ), e, t) ->
+      begin
+        match e with
+        | Add (x, y) ->
+          (match M.find_opt x env with
+           | Some var2 -> Let ((var, typ), Add (var2, y), const_fold_mov env t)
+           | None -> Let ((var, typ), e, const_fold_mov env t))
+        | Sub (x, y) ->
+          (match M.find_opt x env with
+           | Some var2 -> Let ((var, typ), Sub (var2, y), const_fold_mov env t)
+           | None -> Let ((var, typ), e, const_fold_mov env t))
+        | Ld (x, V y, z) ->
+          (match M.find_opt x env with
+           | Some var2 -> Let ((var, typ), Ld (var2, V y, z), const_fold_mov env t)
+           | None ->
+             match M.find_opt y env with
+             | Some var2 -> Let ((var, typ), Ld (x, V var2, z), const_fold_mov env t)
+             | None -> Let ((var, typ), e, const_fold_mov env t))
+        | St (x, y, V z, w) ->
+          (match M.find_opt x env with
+           | Some var2 -> Let ((var, typ), St (var2, y, V z, w), const_fold_mov env t)
+           | None ->
+             match M.find_opt y env with
+             | Some var2 -> Let ((var, typ), St (x, var2, V z, w), const_fold_mov env t)
+             | None ->
+               match M.find_opt z env with
+               | Some var2 -> Let ((var, typ), St (x, y, V var2, w), const_fold_mov env t)
+               | None -> Let ((var, typ), e, const_fold_mov env t))
+        | CallDir (id_l, args, fargs) ->
+          Let ((var, typ),
+               CallDir (id_l,
+                        args |> List.map (fun x -> if M.mem x env then M.find x env else x),
+                        fargs |> List.map (fun x -> if M.mem x env then M.find x env else x)),
+               const_fold_mov env t)
+        | _ -> Let ((var, typ), e, const_fold_mov env t)
+      end
+    | Ans e -> Ans e
+  ;;
 end
 
 
@@ -270,6 +359,41 @@ let%test_module "constfold test" = (module struct
     Ans (CallDir (L "guard_tracetj0.844",["stack.399"; "sp2.683.1423"; "bytecode.401.1293"; "Ti195.686.1424"; ],[])))))))))))))))))))))))))))))
   ;;
 
+  let t_straight_trace2 =
+    Let (("sp.400.1517", Int),  Mov ("sp2.683.1423"),
+    Let (("Ti242.609.1606", Int),  Sub ("sp.400.1517",C 2 ),
+    Let (("Ti244.611.1607", Int),  Sub ("Ti242.609.1606",C 1 ),
+    Let (("v.612.1608", Int),  Ld ("stack.399",V "Ti244.611.1607",4),
+    Let (("Tu24.613.1609", Unit),  St ("v.612.1608","stack.399",V "sp.400.1517",4),
+    Let (("Ti246.615.1610", Int),  Add ("sp.400.1517",C 1 ),
+    Let (("sp.400.1740", Int),  Mov ("Ti246.615.1610"),
+    Let (("c.689.1872", Int),  Set (1),
+    Let (("Tu15.690.1873", Unit),  St ("c.689.1872","stack.399",V "sp.400.1740",4),
+    Let (("Ti184.692.1874", Int),  Add ("sp.400.1740",C 1 ),
+    Let (("sp.400.1963", Int),  Mov ("Ti184.692.1874"),
+    Let (("Ti85.799.2165", Int),  Sub ("sp.400.1963",C 1 ),
+    Let (("v2.800.2166", Int),  Ld ("stack.399",V "Ti85.799.2165",4),
+    Let (("Ti87.802.2167", Int),  Sub ("sp.400.1963",C 2 ),
+    Let (("v1.803.2168", Int),  Ld ("stack.399",V "Ti87.802.2167",4),
+    Let (("Ti89.805.2169", Int),  Sub ("sp.400.1963",C 2 ),
+    Let (("Ti90.806.2170", Int),  Sub ("v1.803.2168",V "v2.800.2166"),
+    Let (("Tu6.807.2171", Unit),  St ("Ti90.806.2170","stack.399",V "Ti89.805.2169",4),
+    Let (("Ti92.809.2172", Int),  Sub ("sp.400.1963",C 1 ),
+    Let (("sp.400.2186", Int),  Mov ("Ti92.809.2172"),
+    Let (("Ti340.500.2201", Int),  Sub ("sp.400.2186",C 1 ),
+    Let (("v.501.2202", Int),  Ld ("stack.399",V "Ti340.500.2201",4),
+    Let (("Tu33.502.2203", Unit),  St ("v.501.2202","stack.399",V "sp.400.2186",4),
+    Let (("Ti342.504.2204", Int),  Add ("sp.400.2186",C 1 ),
+    Let (("sp.400.2409", Int),  Mov ("Ti342.504.2204"),
+    Let (("Ti208.663.2524", Int),  Set (31),
+    Let (("Tu21.664.2525", Unit),  St ("Ti208.663.2524","stack.399",V "sp.400.2409",4),
+    Let (("Ti210.666.2526", Int),  Add ("sp.400.2409",C 1 ),
+    Let (("Ti211.667.2527", Int),  Set (200),
+    Let (("Tu20.668.2528", Unit),  St ("Ti211.667.2527","stack.399",V "Ti210.666.2526",4),
+    Let (("sp2.670.2529", Int),  Add ("sp.400.2409",C 2 ),
+         Ans (CallDir (L "tracetj0.844",["stack.399"; "sp2.670.2529"; ],[])))))))))))))))))))))))))))))))))
+  ;;
+
   let%test "const_fold test1" =
     let r1 = Opt.(const_fold empty_env t_trace1) in
     let r2 = Opt.elim_dead_exp r1 in
@@ -279,7 +403,19 @@ let%test_module "constfold test" = (module struct
     pp "\n[TEST] Applying elim_dead_exp\n";
     r2 |> print_t; print_newline ();
     pp "\n[TEST] Applying const_fold_if\n";
-    r3 |> print_t; print_newline ();
+    r3 |> print_t; print_newline (); print_newline ();
     true
   ;;
+
+  let%test "const_fold_mov test1" =
+    pp "[TEST] cnost_fold_mov test1\n";
+    let r1 = Opt.(const_fold empty_env t_straight_trace2 |> elim_dead_exp) in
+    let r2 = Opt.(r1 |> const_fold_mov empty_env) in
+    let r3 = Opt.(const_fold empty_env r2 |> elim_dead_exp) in
+    print_t r1; print_newline (); print_newline ();
+    print_t r2; print_newline (); print_newline ();
+    print_t r3; print_newline (); print_newline ();
+    true
+  ;;
+
 end)
