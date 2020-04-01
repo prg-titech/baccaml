a0b9c9a57337f7503f03cd0da0ecff7c68fe05c6
Yusuke Izawa <yuizalp@gmail.com>
2020-04-01 19:47:23 +0900

    Updated opt_defuse.ml

--- opt/opt_defuse.ml
+++ opt/opt_defuse.ml
@@ -2,3 +2,15 @@ open Std
 open MinCaml
 open Asm
 open Opt_lib
+
+let f t =
+  Opt_const_fold.(
+    const_fold_exp t
+    |> const_fold_mov
+    |> Opt_guard.move_into_guard
+    |> const_fold_if
+    |> const_fold_id
+    |> elim_dead_exp)
+  |> Opt_mem.const_fold_rw
+  |> Opt_const_fold.elim_dead_exp
+  |> Opt_const_fold.const_fold_mov
