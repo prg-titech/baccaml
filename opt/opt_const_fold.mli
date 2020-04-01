d3c063b9f8987dcb412c5157ad6ab7a1188cc710
Yusuke Izawa <yuizalp@gmail.com>
2020-04-01 19:45:45 +0900

    Updated opt_const_fold.mli

--- opt/opt_const_fold.mli
+++ opt/opt_const_fold.mli
@@ -1,7 +1,9 @@
 open MinCaml
 open Asm
+open Opt_lib
 
+val const_fold_exp : ?env:exp M.t -> t -> t
 val const_fold_mov : ?env:string M.t -> t -> t
-val elim_dead_exp : t -> t
-val const_fold : ?env:exp M.t -> t -> t
+val const_fold_id : ?env:string M.t -> t -> t
 val const_fold_if : ?env:exp M.t -> t -> t
+val elim_dead_exp : t -> t
