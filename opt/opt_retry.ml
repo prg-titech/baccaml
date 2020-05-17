332e28689252a76319fd876fd78d445479bf1d53
Yusuke Izawa <yuizalp@gmail.com>
2020-05-18 01:56:00 +0900

    Insert a comment representing value of pc at guard

--- opt/opt_retry.ml
+++ opt/opt_retry.ml
@@ -26,15 +26,17 @@ let rec rename_guard { pc; tname } env =
   | Let ((var, typ), Sub (x, C y), t) when is_pc x ->
     let pc_v = M.find x env in
     let env = M.add var (pc_v - y) env in
-    Let ((var, typ), Add (x, C y), rename_guard { pc; tname } env t)
+    Let ((var, typ), Sub (x, C y), rename_guard { pc; tname } env t)
   | Let ((var, typ), e, t) ->
     Let ((var, typ), e, rename_guard { pc; tname } env t)
   | Ans (CallDir (Id.L name, args, fargs) as e) ->
     let pc_arg = List.find (fun arg -> M.mem arg env) args in
-    Ans
       (match M.find_opt pc_arg env with
-      | Some pc_v when pc_v = pc -> CallDir (Id.L tname, args, fargs)
-      | Some _ | None -> e)
+       | Some pc_v when pc_v = pc ->
+         Let ((Id.gentmp Type.Unit, Type.Unit),
+              Comment ("guard_pc." ^ (string_of_int pc_v)),
+              Ans (CallDir (Id.L tname, args, fargs)))
+       | Some _ | None -> Ans e)
   | Ans e -> Ans e
 ;;
 
