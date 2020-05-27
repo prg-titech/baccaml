b86a3c1cddefc5be163aedb2c774e6de6c453cbe
Yusuke Izawa <yuizalp@gmail.com>
2020-05-27 16:14:54 +0900

    [runtime] Move reg and mem global in Setup module

--- runtime/runtime_entry.ml
+++ runtime/runtime_entry.ml
@@ -21,7 +21,37 @@ let interp_ir : Asm.prog option ref = ref None
 let interp_fundef : Asm.fundef option ref = ref None
 
 module Setup = struct
+  open Asm
   module I = Config.Internal
+
+  let reg = Array.make !I.size (Red 0)
+  let mem = Array.make !I.size (Green 0)
+
+  let get_ir_addr args name =
+    List.find (fun a -> String.get_name a = name) args
+    |> String.get_extension
+    |> int_of_string
+  ;;
+
+  let make_reg { args; body = t } =
+    let open Jit_env in
+    Asm.fv t @ args
+    |> List.iteri (fun i a ->
+           if List.mem (String.get_name a) !Config.greens
+           then reg.(i) <- Green 0
+           else reg.(i) <- Red 0);
+    reg
+  ;;
+
+  let make_mem ~bc_addr ~st_addr bytecode stack =
+    let open Jit_env in
+    bytecode
+    |> Array.iteri (fun i a -> mem.(bc_addr + (4 * i)) <- Jit_env.Green a);
+    stack
+    |> Array.iteri (fun i a -> mem.(st_addr + (4 * i)) <- Jit_env.Red a);
+    mem
+  ;;
+
   let env { bytecode; stack; pc; sp; bc_ptr; st_ptr } typ interp =
     let open Asm in
     let open Util in
