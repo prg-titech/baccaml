ecf84d3d927b157ab7d495889e456283a126b395
Yusuke Izawa <yuizalp@gmail.com>
2020-06-27 00:25:48 +0900

    Implemented the prototype of C runtime

--- runtime/runtime.c
+++ runtime/runtime.c
@@ -1,7 +1,6 @@
 #define _GNU_SOURCE
 
 #include <assert.h>
-#define CAML_NAME_SPACE
 #include <caml/alloc.h>
 #include <caml/callback.h>
 #include <caml/compatibility.h>
@@ -17,20 +16,29 @@
 #include <sys/time.h>
 #include <unistd.h>
 
-#define PROF_LEN 2048
+#include "interop_caml.h"
+#include "runtime_camlwrap.h"
+
+#define ARR_LEN 2048
 #define THOLD_TJ 100
 #define THOLD_MJ 0
 
 #define JIT_COMPILE_COMMAND "gcc -m32 -fPIC -shared"
 
-typedef int (*fun_arg2)(int, int);
+typedef int (*fun_arg2)(int*, int);
 
 enum jit_type { TJ, MJ };
 
 /**
  * For profiling a program counter
  */
-int prof_arr[1024] = {0};
+int prof_arr[ARR_LEN] = {0};
+
+bool compiled_arr[ARR_LEN] = {false};
+
+char* trace_name_arr[ARR_LEN] = {NULL};
+
+fun_arg2 sym_arr[ARR_LEN] = {NULL};
 
 /**
  * For gen_trace_name
@@ -58,17 +66,34 @@ void gen_trace_name(char *buffer, enum jit_type typ) {
 /**
  * Generate a name for created shared objects
  */
-void gen_so_name(char *buffer, char *tname) {
-  sprintf(buffer, "./lib%s.so", tname);
+void gen_so_name(char* buf, char *trace_name) {
+  char str1[1024] = "./lib";
+  char ext[12] = ".so";
+  strcat(str1, trace_name); // "./libtracetj0.123"
+  strcat(str1, ext); // "./libtracetj0.123.so"
+  strcpy(buf, str1);
+  return;
+}
+
+void strip_ext(char *fname) {
+  char *end = fname + strlen(fname);
+
+  while (end > fname && *end != '.') {
+    --end;
+  }
+
+  if (end > fname) {
+    *end = '\0';
+  }
 }
 
 /**
  * Compile a trace into shared object
  */
-void jit_compile(char *so, char *func, int pc) {
+void jit_compile(char *so, char *func) {
   char buffer[1024];
 
-  printf("compiling shared object: %s\n", so);
+  printf("compiling trace %s into shared object %s\n", func, so);
   sprintf(buffer, "%s -c %s.s", JIT_COMPILE_COMMAND, func);
   system(buffer);
 
@@ -79,20 +104,73 @@ void jit_compile(char *so, char *func, int pc) {
   return;
 }
 
-int bytecode[1024] = {0};
+
+
+/**
+ * Profiling how many back-edge insertions occur.
+ */
+void c_can_enter_jit(int *stack, int sp, int *code, int pc) {
+  //printf("back edge at pc %d\n", pc);
+  if (pc != 96)
+    prof_arr[pc]++;
+  return;
+}
 
 /**
  * Entry point of jitting.
  * TODO: change the arguments of jit_merge_point in interp.mcml
  */
-void call_caml_jit_merge_point(int pc, int* stack, int sp) {
+void c_jit_merge_point(int* stack, int sp, int* code, int pc) {
   int pc_count;
+  char trace_name[128];
+  char so_name[1024];
+  void* handle = NULL;
+  fun_arg2 sym = NULL;
 
-  pc_count = prof_arr[pc]++;
+  pc_count = prof_arr[pc];
+  if (pc_count < THOLD_TJ) {
+    // exit if pc_count is under THOLD
+    return;
+  } else {
+    //printf("over thold at pc %d\n", pc);
+    //call_caml_jit_entry(stack, sp, code, pc);
 
-  // exit if pc_count is under THOLD
-  if (pc_count < THOLD_TJ) return;
+    if (!compiled_arr[pc] ) {
+      strcpy(trace_name, call_caml_jit_tracing(stack, sp, code, pc));
+      trace_name_arr[pc] = malloc(128*sizeof(char));
+      strcpy(trace_name_arr[pc], trace_name);
 
-  printf("over thold\n");
-  return;
+      gen_so_name(so_name, trace_name);
+      jit_compile(so_name, trace_name);
+      compiled_arr[pc] = true;
+    }
+
+    if (sym_arr[pc] == NULL) {
+      strcpy(trace_name, trace_name_arr[pc]);
+      gen_so_name(so_name, trace_name);
+
+      strip_ext(trace_name);
+      handle = dlopen(so_name, RTLD_NOW | RTLD_GLOBAL);
+      if (handle == NULL) {
+        fprintf(stderr, "error: dlopen %s\n", so_name);
+        exit(-1);
+      }
+      dlerror();
+
+      sym = (fun_arg2)dlsym(RTLD_DEFAULT, trace_name);
+      if (sym == NULL) {
+        fprintf(stderr, "error: dlsym \n");
+        return;
+      }
+      sym_arr[pc] = malloc(sizeof(fun_arg2));
+      sym_arr[pc] = sym;
+      sym(stack, sp);
+      printf("execution finished at pc %d\n", pc);
+      return;
+    } else {
+      sym = sym_arr[pc];
+      sym(stack, sp);
+      return;
+    }
+  }
 }
