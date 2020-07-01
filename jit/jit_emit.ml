9c9b5cce5b7201130a329d16ff3ba29d732c9e63
Yusuke Izawa <yuizalp@gmail.com>
2020-07-01 22:55:49 +0900

    Store the return value to the stack (method jit)

--- jit/jit_emit.ml
+++ jit/jit_emit.ml
@@ -7,11 +7,7 @@ let emit_mj
     ({ name = Id.L x; args; fargs = _; body = e; ret = _ } as fundef)
   =
   let cname = Filename.chop_extension x in
-  let mj_result = "res." ^ x in
   fprintf oc ".code32\n";
-  fprintf oc ".data\n";
-  fprintf oc "%s:\n" mj_result;
-  fprintf oc "\t.long\t0x0\n";
   fprintf oc ".balign\t8\n";
   fprintf oc ".text\n";
   fprintf oc ".globl %s\n" cname;
@@ -26,7 +22,7 @@ let emit_mj
   fprintf oc "\tmovl\t32(%%esp),%s\n" regs.(0);
   fprintf oc "\tmovl\t36(%%esp),%s\n" regs.(1);
   fprintf oc "\tcall\t%s\n" x;
-  fprintf oc "\tmovl\t%%eax, %s\n" mj_result;
+  fprintf oc "\tmovl\t%%eax, 32(%%esp)\n";
   fprintf oc "\tpopl\t%%ebp\n";
   fprintf oc "\tpopl\t%%edi\n";
   fprintf oc "\tpopl\t%%esi\n";
@@ -34,7 +30,7 @@ let emit_mj
   fprintf oc "\tpopl\t%%ecx\n";
   fprintf oc "\tpopl\t%%ebx\n";
   fprintf oc "\tpopl\t%%eax\n";
-  fprintf oc "\tmovl\t%s, %%eax\n" mj_result;
+  fprintf oc "\tmovl\t4(%%esp), %%eax\n";
   fprintf oc "\tret\n";
   Emit.h oc fundef;
   fprintf oc "guard_%s:\n" x;
