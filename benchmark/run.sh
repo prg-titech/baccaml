1bb9929024c592d3df5d370aa6d2504dd2fd06b9
Yusuke Izawa <yuizalp@gmail.com>
2020-07-03 01:01:21 +0900

    Updated run.sh

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -6,14 +6,29 @@ existsFile() {
 
 allTests=(ack tak sieve ary sum sum-tail fib fact prefix_sum random)
 
-echo "[Benchmark] MinCaml"
+run_mincaml() {
+  echo "[Benchmark] MinCaml"
 
-for target in ${allTests[@]}; do
-  echo "executing ${target}";
-  cd mincaml--/ && ./build.ml test/${target}_time.ml 2>/dev/null && cd ..
-  file=benchmark/${target}_mincaml.data
-  if existsFile ${file}; then rm ${file}; fi
-  for i in `seq 1 100`; do
-    ./mincaml--/test/${target}_time.exe >> benchmark/${target}_mincaml.data
+  for target in ${allTests[@]}; do
+    echo "executing ${target}";
+    cd mincaml--/ && ./build.ml test/${target}_time.ml 2>/dev/null && cd ..
+    file=benchmark/${target}_mincaml.data
+    if existsFile ${file}; then rm ${file}; fi
+    for i in `seq 1 100`; do
+      ./mincaml--/test/${target}_time.exe >> benchmark/${target}_mincaml.data
+    done
   done
-done
+}
+
+run_baccaml_tj() {
+  echo "[Benchmark] BacCaml Tracing JIT"
+
+  for target in ${allTests[@]}; do
+    echo "executing ${target}"
+    file=benchmark/${target}_tj.data
+    existsFile ${file} && rm ${file}
+    bac-caml mincaml--/test/${target}.ml | dune exec interp/interp_tj.exe -- interp/interp.mcml >> benchmark/${target}_tj.data
+  done
+}
+
+run_mincaml
